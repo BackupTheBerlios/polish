4764118e53bcb0faac53fcba975d8fbbbaa2e20b
enough <enough>
2004-09-27 11:08:52 +0000

    When libraries were extracted, directories were extracted as files. This is now fixed.

--- enough-polish-build/source/src/de/enough/polish/util/JarUtil.java
+++ enough-polish-build/source/src/de/enough/polish/util/JarUtil.java
@@ -150,19 +150,21 @@ public final class JarUtil {
 		Enumeration enum = input.entries();
 		for (; enum.hasMoreElements();) {
 			JarEntry entry = (JarEntry) enum.nextElement();
-			String path = targetPath + entry.getName();
-			File file = new File( path );
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
+			if (!entry.isDirectory()) {
+				String path = targetPath + entry.getName();
+				File file = new File( path );
+				if (!file.getParentFile().exists()) {
+					file.getParentFile().mkdirs();
+				}
+				FileOutputStream out = new FileOutputStream( file);
+				InputStream in = input.getInputStream(entry);
+				int read;
+				while ( (read = in.read( buffer )) != -1) {
+					out.write( buffer, 0, read );
+				}
+				in.close();
+				out.close();
 			}
-			FileOutputStream out = new FileOutputStream( file);
-			InputStream in = input.getInputStream(entry);
-			int read;
-			while ( (read = in.read( buffer )) != -1) {
-				out.write( buffer, 0, read );
-			}
-			in.close();
-			out.close();
 		}
 		
 	}
