d6a8a221f0a4d50c045e0d09af35ba24a1d6d2f3
rickyn <rickyn>
2005-03-01 17:11:59 +0000

    Not a real unittest. For visual testing only (e.i. looking at the output)

---
+++ eclipsePlugin/source/test/de/enought/polish/plugin/eclipse/css/parser/CssTokenizerTest.java
@@ -0,0 +1,104 @@
+package de.enought.polish.plugin.eclipse.css.parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.List;
+
+import de.enough.polish.plugin.eclipse.css.parser.CssToken;
+import de.enough.polish.plugin.eclipse.css.parser.CssTokenizer;
+import junit.framework.TestCase;
+
+/*
+ * Created on Mar 1, 2005 at 10:15:22 AM.
+ * 
+ * Copyright (c) 2005 Robert Virkus / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
+
+/**
+ * <p>This testclass is not a unittest. Its for visual debugging only. That means looking at the output and
+ * let the brain decide if it is correct or not.</p>
+ *
+ * <p>Copyright Enough Software 2005</p>
+ * <pre>
+ * history
+ *        Mar 1, 2005 - ricky creation
+ * </pre>
+ * @author Robert Virkus, j2mepolish@enough.de
+ */
+public class CssTokenizerTest extends TestCase {
+
+	String input;
+	
+	public void setUp(){
+		System.out.println("CssTokenizerTest.setUp().enter.");
+		
+		String cssFile = "/Users/ricky/workspace/enough-polish-plugin-eclipse/misc/example.css";
+		
+		BufferedReader bufferedReader;
+		this.input = "";
+		File file = new File(cssFile);
+		try{
+			bufferedReader = new BufferedReader(new FileReader(file));
+			StringBuffer buffer = new StringBuffer();
+			String line;
+			while((line=bufferedReader.readLine())!=null){
+				buffer.append(line).append('\n');
+			}
+			this.input = buffer.toString();
+		}
+		catch(Exception exception){
+			System.out.println("CssTokenizerTest.setUp().File not found:"+cssFile);
+			fail();
+			return;
+		}
+	}
+	
+	public void XtestGetNext(){
+		CssTokenizer cssTokenizer = new CssTokenizer(this.input);
+		CssToken cssToken;
+		cssToken = cssTokenizer.getNextToken();
+		System.out.println(cssToken);
+		cssToken = cssTokenizer.getNextToken();
+		System.out.println(cssToken);
+		cssToken = cssTokenizer.getNextToken();
+		System.out.println(cssToken);
+	}
+	
+	public void testTokenize(){
+		
+		CssTokenizer cssTokenizer = new CssTokenizer(this.input);
+		cssTokenizer.tokenize();
+		List cssTokens = cssTokenizer.getCssTokens();
+		
+		System.out.println("CssTokenizerTest.testTokenize():enter.");
+		Iterator i = cssTokens.iterator();
+		while(i.hasNext()){
+			CssToken cssToken = (CssToken)i.next();
+			System.out.println(cssToken);
+		}
+		System.out.println("CssTokenizerTest.testTokenize():# of tokens:"+cssTokens.size());
+		System.out.println("CssTokenizerTest.testTokenize():end.");
+	}
+}
