bb1c31d9c946a543b965c01adbcdb36d1cbec844
rickyn <rickyn>
2006-01-11 17:09:27 +0000

    accessable parent added. change of selection modifies children and parents appropriate

--- build/source/src/de/enough/polish/devices/DeviceTreeItem.java
+++ build/source/src/de/enough/polish/devices/DeviceTreeItem.java
@@ -26,6 +26,7 @@
 package de.enough.polish.devices;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.enough.polish.Device;
@@ -48,16 +49,21 @@ public class DeviceTreeItem {
 	private final Vendor vendor;
 	private final Device device;
 	private final List children;
-	
-
+    private DeviceTreeItem parentItem;
+//    private LinkedList selectionListeners;
+//    public interface SelectionListener{
+//        //
+//    }
+    
 	/**
 	 * Creates a new tree item.
 	 * 
-	 * 
+	 * @param parentItem the parent of this item, can be null
 	 * @param vendor the current vendor, can be null
 	 * @param device the mapped device, can be null
 	 */
-	public DeviceTreeItem( Vendor vendor, Device device ) {
+	public DeviceTreeItem(DeviceTreeItem parentItem, Vendor vendor, Device device ) {
+        this.parentItem = parentItem;
 		this.children = new ArrayList();
 		this.vendor = vendor;
 		this.device = device;
@@ -72,22 +78,59 @@ public class DeviceTreeItem {
 		} else {
 			this.name = "Virtual";
 		}
+//        this.selectionListeners = new LinkedList();
 	}
 	
 	public DeviceTreeItem[] getChildren() {
 		return (DeviceTreeItem[]) this.children.toArray( new DeviceTreeItem[ this.children.size() ] );
 	}
 	
-	public void select( boolean selected ) {
-		this.isSelected = selected;
+	public void setIsSelected( boolean selected ) {
+        
+        setIsSelectedOnItemOnly(selected);
+        
 		DeviceTreeItem[] childs = getChildren();
 		for (int i = 0; i < childs.length; i++) {
 			DeviceTreeItem item = childs[i];
-			item.select(selected);
+			item.setIsSelectedOnItemAndChildren(selected);
 		}
+        
+		// Uncheck the parent if we become unchecked as the parent can only be checked
+        // if every child is checked. 
+		if(selected == false) {
+            DeviceTreeItem parent = getParentItem();
+            if(parent != null) {
+                parent.setIsSelectedOnItemAndParent(selected);
+            }
+        }
 	}
 	
-	public boolean isSelected() {
+    private void setIsSelectedOnItemAndParent(boolean selected) {
+        setIsSelectedOnItemOnly(selected);
+        DeviceTreeItem parent = getParentItem();
+        if(parent != null) {
+            parent.setIsSelectedOnItemAndParent(selected);
+        }
+    }
+    
+    private void setIsSelectedOnItemAndChildren(boolean selected) {
+        setIsSelectedOnItemOnly(selected);
+        DeviceTreeItem[] childrenTemp = getChildren();
+        for (int i = 0; i < childrenTemp.length; i++) {
+            DeviceTreeItem item = childrenTemp[i];
+            item.setIsSelectedOnItemAndChildren(selected);
+        }
+    }
+    
+	/**
+     * Marks this item as selected but does not alter the state of its children.
+     * @param selected true if this item is selected, false otherwise.
+     */
+    private void setIsSelectedOnItemOnly(boolean selected) {
+        this.isSelected = selected;
+    }
+
+    public boolean isSelected() {
 		return this.isSelected;
 	}
 	
@@ -124,7 +167,37 @@ public class DeviceTreeItem {
 	protected void addChild( DeviceTreeItem child ) {
 		this.children.add( child );
 	}
+
+    /**
+     * Returns the parent item of this item.
+     * @return the parent this item or null if is has none.
+     */
+    public DeviceTreeItem getParentItem() {
+        return this.parentItem;
+    }
+
+    /**
+     * Sets a new parent of this item.
+     * @param parentItem the new parent of this item or null it it has none.
+     */
+    public void setParentItem(DeviceTreeItem parentItem) {
+        this.parentItem = parentItem;
+    }
 	
-	
+//    private void fireSelectionChange() {
+//        for (Iterator iterator = this.selectionListeners.iterator(); iterator.hasNext(); ) {
+//            SelectionListener selectionListener = (SelectionListener) iterator.next();
+//            selectionListener.
+//        }
+//    }
+//	
+//    // TODO: Use the generic EventManager.
+//    public void addSelectionListener(SelectionListener selectionListener) {
+//        this.selectionListeners.add(selectionListener);
+//    }
+//    
+//    public void removeSelectionListener(SelectionListener selectionListener) {
+//        this.selectionListeners.remove(selectionListener);
+//    }
 
 }
