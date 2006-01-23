f2eadc2bdd0a2da3c88e7b11abed6d735df3257f
enough <enough>
2006-01-23 14:27:03 +0000

    Additional "inactive" attribute for ChoiceItems

--- build/source/src/de/enough/polish/ant/buildlistener/BuildListenerExtensionSetting.java
+++ build/source/src/de/enough/polish/ant/buildlistener/BuildListenerExtensionSetting.java
@@ -3,5 +3,5 @@ package de.enough.polish.ant.buildlistener;
 import de.enough.polish.ExtensionSetting;
 
 public class BuildListenerExtensionSetting extends ExtensionSetting {
-
+	// no special settings so far
 }
