e998612a5da5fa953e66b33bf5eea9d45349b2cf
enough <enough>
2006-03-24 16:25:54 +0000

    Updated snapshot.

--- enough-polish-build/source/src/de/enough/polish/ant/PolishTask.java
+++ enough-polish-build/source/src/de/enough/polish/ant/PolishTask.java
@@ -130,7 +130,7 @@ import de.enough.polish.util.TextFileManager;
  */
 public class PolishTask extends ConditionalTask {
 
-	private static final String VERSION = "1.3<beta4-preview>";
+	private static final String VERSION = "1.3<beta4-preview> (2006-03-23)";
 
 	private BuildSetting buildSetting;
 	private InfoSetting infoSetting;
