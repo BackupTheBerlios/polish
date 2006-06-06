39df04dbd52bf9037d55feeaf282595eb8b76277
rickyn <rickyn>
2006-06-06 15:33:50 +0000

    the defaults are not sane but at least correct

--- enough-mepose-core/source/src/de/enough/mepose/core/preferences/PreferenceInitializer.java
+++ enough-mepose-core/source/src/de/enough/mepose/core/preferences/PreferenceInitializer.java
@@ -13,8 +13,8 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
 	public void initializeDefaultPreferences() {
 		Preferences store = MeposePlugin.getDefault().getPluginPreferences();
-		store.setDefault(MeposeConstants.POLISH_INSTALLATION_DIR,"hallo");
-		store.setDefault(MeposeConstants.WTK_INSTALLATION_DIR,"welt");
+		store.setDefault(MeposeConstants.POLISH_INSTALLATION_DIR,"/");
+		store.setDefault(MeposeConstants.WTK_INSTALLATION_DIR,"/");
         
 	}
 
