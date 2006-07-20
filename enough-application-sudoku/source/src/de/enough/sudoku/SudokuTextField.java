8767aa2b7ad4359d45a9a79676211891eb146fe0
sschmitt <sschmitt>
2006-07-20 12:22:24 +0000

    this is a modified version of the TextField, which is implemented to represent sudoku cells

---
+++ enough-application-sudoku/source/src/de/enough/sudoku/SudokuTextField.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package de.enough.sudoku;
+
+
+import javax.microedition.lcdui.Canvas;
+
+import de.enough.polish.ui.Style;
+import de.enough.polish.ui.TextField;
+
+/**
+ * @author jinny
+ *
+ */
+public class SudokuTextField
+//#if polish.usePolishGui
+	//# extends TextField
+//#else
+	extends de.enough.polish.ui.FakeTextFieldCustomItem
+//#endif
+{
+
+	private final int minimum;
+	private final int maximum;
+
+	public SudokuTextField( int minimum, int maximum ) {
+		this( minimum, maximum, null );
+	}
+
+	public SudokuTextField( int minimum, int maximum,Style style  ) {
+		super(null, null, 1, TextField.NUMERIC, style );
+		this.minimum = minimum;
+		this.maximum = maximum;
+		this.showCaret = false;
+	}
+	
+	public void setValue( int value ) {
+		setString( "" + value );
+	}
+	
+	public int getValue() {
+		String strValue = getString();
+		if (strValue.length() == 0) {
+			return -1;
+		}
+		return Integer.parseInt( strValue  );
+	}
+
+
+	protected boolean handleKeyPressed(int keyCode, int gameAction) {
+		if (this.isUneditable) {
+			return false;
+		}
+		if (keyCode >= Canvas.KEY_NUM1 && keyCode <= Canvas.KEY_NUM9 ) {
+			setString( Integer.toString( keyCode - Canvas.KEY_NUM0 ) );
+			notifyStateChanged();
+			this.flashCaret = false;
+			return true;
+		}
+		return false;
+	}
+
+	
+	
+
+}
