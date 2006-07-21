9d4723898d749964e205574a740998b9ea2d77db
sschmitt <sschmitt>
2006-07-21 15:39:42 +0000

    this shall become the outsourced display of the SudikuBoard, which can prepare/load itself in the background as a thread

---
+++ enough-application-sudoku/source/src/de/enough/sudoku/GameForm.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package de.enough.sudoku;
+
+import javax.microedition.lcdui.Form;
+
+/**
+ *
+ * 
+ * @author Simon Schmitt
+ *
+ */
+
+
+/* TODO: This class shall 
+	- handle the loading/generation of a new Board
+	- contain the Textfields
+	- tell the waitMenu/Gauge when to hide
+
+*/
+public class GameForm extends Form implements Runnable{
+	
+	public GameForm(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run(){
+		//TODO show the Board or generate a new
+		
+		// TODO show your shelf (Gauge will hide)
+	}
+}
