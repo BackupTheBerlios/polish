fdeeeebff7853d668054e8b3059410d9dbe8c18f
rickyn <rickyn>
2006-10-06 12:07:27 +0000

    new menu for polish actions

---
+++ enough-mepose-ui/source/src/de/enough/mepose/actions/BuildSeveralDevicesActionDelegate.java
@@ -0,0 +1,55 @@
+/*
+ * Created on Oct 5, 2006 at 6:02:15 PM.
+ * 
+ * Copyright (c) 2005 Robert Virkus / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with J2ME Polish; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
+package de.enough.mepose.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.actions.ActionDelegate;
+
+/**
+ * 
+ * <br>Copyright Enough Software 2005
+ * <pre>
+ * history
+ *        Oct 5, 2006 - rickyn creation
+ * </pre>
+ * @author Richard Nkrumah, Richard.Nkrumah@enough.de
+ */
+public class BuildSeveralDevicesActionDelegate extends ActionDelegate{
+
+    public void runWithEvent(IAction action, Event event) {
+        super.runWithEvent(action, event);
+        System.out.println("DEBUG:BuildSeveralDevicesActionDelegate.runWithEvent(...):enter.");
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+        super.selectionChanged(action, selection);
+    }
+
+    
+    
+}
