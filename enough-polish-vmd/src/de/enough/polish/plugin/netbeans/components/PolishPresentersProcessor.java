4f11d10287760f69bffd5b3cf85a0a891a795da4
dkaspar <dkaspar>
2007-03-28 12:17:26 +0000

    Added polish processors for properties and presenters

---
+++ enough-polish-vmd/src/de/enough/polish/plugin/netbeans/components/PolishPresentersProcessor.java
@@ -0,0 +1,55 @@
+/*
+ * PolishPresentersProcessor.java
+ *
+ * Created on March 28, 2007, 1:59 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package de.enough.polish.plugin.netbeans.components;
+
+import java.util.ArrayList;
+import org.netbeans.modules.vmd.api.codegen.MultiGuardedSection;
+import org.netbeans.modules.vmd.api.codegen.MultiGuardedSection;
+import org.netbeans.modules.vmd.api.model.ComponentDescriptor;
+import org.netbeans.modules.vmd.api.model.DesignDocument;
+import org.netbeans.modules.vmd.api.model.Presenter;
+import org.netbeans.modules.vmd.api.model.PresentersProcessor;
+import org.netbeans.modules.vmd.api.properties.DefaultPropertiesPresenter;
+import org.netbeans.modules.vmd.midp.codegen.CodeClassInitHeaderFooterPresenter;
+import org.netbeans.modules.vmd.midp.components.MidpDocumentSupport;
+import org.netbeans.modules.vmd.midp.components.MidpTypes;
+import org.netbeans.modules.vmd.midp.components.displayables.DisplayableCD;
+import org.netbeans.modules.vmd.midp.propertyeditors.PropertyEditorString;
+
+/**
+ * @author dave
+ */
+public class PolishPresentersProcessor extends PresentersProcessor {
+    
+    public static final String CATEGORY_POLISH = "J2ME Polish Properties";
+    
+    public PolishPresentersProcessor() {
+        super (MidpDocumentSupport.PROJECT_TYPE_MIDP);
+    }
+    
+    protected void postProcessPresenters(DesignDocument document, ComponentDescriptor descriptor, ArrayList<Presenter> presenters) {
+        if (document.getDescriptorRegistry().isInHierarchy(DisplayableCD.TYPEID, descriptor.getTypeDescriptor().getThisType())) {
+            presenters.add (new DefaultPropertiesPresenter ()
+                .addPropertiesCategory(CATEGORY_POLISH)
+                    .addProperty ("J2ME Polish Style ID", "The style id specific for J2ME Polish", PropertyEditorString.createInstance(), PolishPropertiesProcessor.PROP_DISPLAYABLE_STYLE)
+            );
+            presenters.add (new CodeClassInitHeaderFooterPresenter () {
+                public void generateClassInitializationHeader(MultiGuardedSection section) {
+                    String style = MidpTypes.getString (getComponent().readProperty(PolishPropertiesProcessor.PROP_DISPLAYABLE_STYLE));
+                    if (style != null)
+                        section.getWriter().write ("//#style " + style + "\n");
+                }
+                public void generateClassInitializationFooter(MultiGuardedSection section) {
+                }
+            });
+        }
+    }
+
+}
---
+++ enough-polish-vmd/src/de/enough/polish/plugin/netbeans/components/PolishPropertiesProcessor.java
@@ -0,0 +1,43 @@
+/*
+ * PolishPropertiesProcessor.java
+ *
+ * Created on March 28, 2007, 1:49 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package de.enough.polish.plugin.netbeans.components;
+
+import java.util.ArrayList;
+import org.netbeans.modules.vmd.api.model.ComponentDescriptor;
+import org.netbeans.modules.vmd.api.model.PropertiesProcessor;
+import org.netbeans.modules.vmd.api.model.PropertyDescriptor;
+import org.netbeans.modules.vmd.api.model.PropertyValue;
+import org.netbeans.modules.vmd.api.model.Versionable;
+import org.netbeans.modules.vmd.midp.components.MidpDocumentSupport;
+import org.netbeans.modules.vmd.midp.components.MidpTypes;
+import org.netbeans.modules.vmd.midp.components.displayables.DisplayableCD;
+
+/**
+ * @author dave
+ */
+public class PolishPropertiesProcessor extends PropertiesProcessor {
+    
+    public static final String PROP_DISPLAYABLE_STYLE = "j2mepolish.style"; // NOI18N
+    
+    public PolishPropertiesProcessor() {
+        super (MidpDocumentSupport.PROJECT_TYPE_MIDP);
+    }
+    
+    protected void postProcessProperties(ComponentDescriptor descriptor, ArrayList<PropertyDescriptor> properties) {
+        while (descriptor != null) {
+            if (DisplayableCD.TYPEID.equals (descriptor.getTypeDescriptor().getThisType())) {
+                properties.add(new PropertyDescriptor (PROP_DISPLAYABLE_STYLE, MidpTypes.TYPEID_JAVA_LANG_STRING, PropertyValue.createNull(), true, false, Versionable.FOREVER));
+                return;
+            }
+            descriptor = descriptor.getSuperDescriptor();
+        }
+    }
+
+}
