6a870fb09801c377e32a7065391f0db04b53df54
enough <enough>
2007-03-28 18:14:25 +0000

    added d'n'd support for borders

--- enough-polish-vmd/src/de/enough/polish/plugin/netbeans/PolishDataEditorVisual.java
+++ enough-polish-vmd/src/de/enough/polish/plugin/netbeans/PolishDataEditorVisual.java
@@ -13,10 +13,14 @@ import de.enough.polish.runtime.Simulation;
 import de.enough.polish.runtime.SimulationDevice;
 import de.enough.polish.runtime.SimulationPanel;
 import de.enough.polish.ui.Background;
+import de.enough.polish.ui.Border;
 import de.enough.polish.ui.Screen;
+import de.enough.polish.ui.Style;
+import de.enough.polish.ui.StyleSheet;
 import de.enough.polish.ui.UiAccess;
 import java.awt.BorderLayout;
 import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetAdapter;
@@ -70,20 +74,70 @@ class SimulationDropListener extends DropTargetAdapter {
          */
         public void drop(DropTargetDropEvent event) {
                 final Transferable transferable = event.getTransferable(); 
+                DataFlavor[] flavors = transferable.getTransferDataFlavors();
+                for (int i=0; i < flavors.length; i++ ) {
+                    System.out.println( flavors[i] );
+                }
                 if (transferable.isDataFlavorSupported(Simulation.BACKGROUND_DATA_FLAVOR)) {
-                        event.acceptDrop(DnDConstants.ACTION_COPY);
+                        System.out.println("data flavor supported...");
+                    
                         try {
                                 final Background background = (Background)transferable.getTransferData( Simulation.BACKGROUND_DATA_FLAVOR );
-                                UiAccess.setBackground( (Screen)simulation.getCurrentDisplayable(), background);
-                                // Signal transfer success.
-                                event.dropComplete(true);
+                                System.out.println("got background: " + background);
+                                if (background != null) {
+                                    event.acceptDrop(DnDConstants.ACTION_COPY);
+                                    UiAccess.setBackground( (Screen)simulation.getCurrentDisplayable(), background);
+                                    // Signal transfer success.
+                                    event.dropComplete(true);
+                                    return;
+                                }
                         } catch (Exception e) {
                                 // Signal transfer failure.
                                 event.dropComplete(false); 
                         }
-                } else {
-                        event.rejectDrop();
                 } 
+                if (transferable.isDataFlavorSupported(Simulation.BORDER_DATA_FLAVOR)) {
+                        System.out.println("data flavor supported...");
+                    
+                        try {
+                                final Border border = (Border)transferable.getTransferData( Simulation.BORDER_DATA_FLAVOR );
+                                System.out.println("got border: " + border);
+                                if (border != null) {
+                                    event.acceptDrop(DnDConstants.ACTION_COPY);
+                                    Screen screen = (Screen)simulation.getCurrentDisplayable();
+                                    Style existing = UiAccess.getStyle( screen );
+                                    if (existing == null) {
+                                        existing = StyleSheet.defaultStyle;
+                                    }
+                                    Style style = new Style(
+                                            0,0,0,0, // margin
+                                            1,1,1,1,1,1, // padding
+                                            existing.layout,
+                                            existing.fontColor,
+                                            existing.font,
+                                            existing.background,
+                                            border,
+                                            null, null
+                                            );
+                                    UiAccess.setStyle( screen, style);
+                                    screen.repaint();
+                                    // Signal transfer success.
+                                    event.dropComplete(true);
+                                    return;
+                                }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                                // Signal transfer failure.
+                                event.dropComplete(false); 
+                        }
+                }
+                System.out.println("data flavor not supported...");
+                event.rejectDrop();
+ 
         }
+        
+        
 		
-	}
+}
+
+
