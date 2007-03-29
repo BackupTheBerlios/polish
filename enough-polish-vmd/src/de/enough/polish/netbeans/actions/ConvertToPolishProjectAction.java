9c3dbf180e5012e3c91f631e487a8e99f55b41b1
dkaspar <dkaspar>
2007-03-29 17:19:49 +0000

    Added ConvertToPolishProjectAction

---
+++ enough-polish-vmd/src/de/enough/polish/netbeans/actions/ConvertToPolishProjectAction.java
@@ -0,0 +1,50 @@
+/*
+ * ConvertToPolishProjectAction.java
+ *
+ * Created on March 29, 2007, 6:38 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package de.enough.polish.netbeans.actions;
+
+import de.enough.polish.netbeans.J2mePolishProjectWizardIterator;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+import org.openide.util.actions.SystemAction;
+
+/**
+ * @author dave
+ */
+public class ConvertToPolishProjectAction extends SystemAction {
+    
+    public ConvertToPolishProjectAction() {
+        putValue("noIconInMenu", Boolean.TRUE); // NOI18N
+    }
+    
+    public String getName() {
+        return "Convert a JavaME project to J2ME Polish project";
+    }
+
+    public HelpCtx getHelpCtx() {
+        return new HelpCtx (ConvertToPolishProjectAction.class);
+    }
+
+    public void actionPerformed(ActionEvent ev) {
+        WizardDescriptor desc = new WizardDescriptor (J2mePolishProjectWizardIterator.createIterator());
+        Dialog dia = DialogDisplayer.getDefault().createDialog(desc);
+        dia.setVisible (true);
+//        Project project = Utilities.actionsGlobalContext().lookup(Project.class);
+//        if (! "J2MEProject".equals (project.getClass().getSimpleName())) { // TODO - HACK because of J2MEProject is not in API
+//            DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message ("Select Mobile Project in Projects window before invoking this action.")); // TODO
+//            return;
+//        }
+//        // TODO - use project.getProjectDirectory()
+//        DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message ("Not implemented yet!"));
+    }
+    
+}
