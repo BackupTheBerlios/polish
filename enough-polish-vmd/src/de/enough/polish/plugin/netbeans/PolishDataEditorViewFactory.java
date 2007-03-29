fe3e4a9ba3d493015df8b3a2be0e3268ed95df12
dkaspar <dkaspar>
2007-03-29 08:16:45 +0000

    PolishDataEditorView is used only for polish projects - means: polish.project.useJ2MEPolish property is set to true or yes in project.properties file

--- enough-polish-vmd/src/de/enough/polish/plugin/netbeans/PolishDataEditorViewFactory.java
+++ enough-polish-vmd/src/de/enough/polish/plugin/netbeans/PolishDataEditorViewFactory.java
@@ -9,10 +9,14 @@
 
 package de.enough.polish.plugin.netbeans;
 
+import org.netbeans.api.project.Project;
 import org.netbeans.modules.vmd.api.io.DataEditorView;
 import org.netbeans.modules.vmd.api.io.DataEditorViewFactory;
 import org.netbeans.modules.vmd.api.io.DataObjectContext;
+import org.netbeans.modules.vmd.api.io.ProjectUtils;
 import org.netbeans.modules.vmd.midp.components.MidpDocumentSupport;
+import org.netbeans.spi.project.support.ant.AntProjectHelper;
+import org.netbeans.spi.project.support.ant.EditableProperties;
 
 /**
  *
@@ -20,10 +24,24 @@ import org.netbeans.modules.vmd.midp.components.MidpDocumentSupport;
  */
 public class PolishDataEditorViewFactory implements DataEditorViewFactory {
     
+    private static final String PROP_USE_POLISH_PROJECT = "polish.project.useJ2MEPolish"; // NOI18N
+    
     public DataEditorView createEditorView(DataObjectContext context) {
-        if (MidpDocumentSupport.PROJECT_TYPE_MIDP.equals (context.getProjectType()))
+        if (MidpDocumentSupport.PROJECT_TYPE_MIDP.equals (context.getProjectType())  &&  isPolishProjectUsed (context))
             return new PolishDataEditorView (context);
         return null;
+    }
+    
+    private static boolean isPolishProjectUsed (DataObjectContext context) {
+        Project project = ProjectUtils.getProject(context);
+        if (project == null)
+            return false;
+        AntProjectHelper helper = project.getLookup ().lookup (AntProjectHelper.class);
+        if (helper == null)
+            return false;
+        EditableProperties ep = helper.getProperties (AntProjectHelper.PROJECT_PROPERTIES_PATH);
+        String value = ep.getProperty (PROP_USE_POLISH_PROJECT);
+        return "true".equalsIgnoreCase(value)  ||  "yes".equalsIgnoreCase(value);
     }
 
 }
