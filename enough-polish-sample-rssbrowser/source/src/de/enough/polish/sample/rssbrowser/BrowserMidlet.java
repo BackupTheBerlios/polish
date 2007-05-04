c34b59f5b50aede26f976f1617c2a70b426932a0
mkoch <mkoch>
2007-05-04 12:55:41 +0000

    - moved item command handling to midlet class. - handle rss item links.

--- enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rssbrowser/BrowserMidlet.java
+++ enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rssbrowser/BrowserMidlet.java
@@ -1,5 +1,13 @@
 package de.enough.polish.sample.rssbrowser;
 
+import de.enough.polish.browser.html.HtmlTagHandler;
+import de.enough.polish.browser.rss.RssItem;
+import de.enough.polish.browser.rss.RssTagHandler;
+import de.enough.polish.ui.Item;
+import de.enough.polish.ui.ItemCommandListener;
+
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
@@ -9,20 +17,24 @@ import javax.microedition.midlet.MIDletStateChangeException;
 
 public class BrowserMidlet 
 	extends MIDlet
-	implements CommandListener
+	implements CommandListener, ItemCommandListener
 {
-	
 	private Command cmdBack = new Command("Back", Command.SCREEN, Command.BACK );
+
+	private Display display;
 	private BrowserScreen browserScreen;
 
-     protected void startApp() throws MIDletStateChangeException{
-          Display display = Display.getDisplay( this );
+     protected void startApp()
+     	throws MIDletStateChangeException
+     {
+          this.display = Display.getDisplay(this);
+
           //#style browserScreen
-          BrowserScreen screen = new BrowserScreen(display, "BrowserItem");
+          BrowserScreen screen = new BrowserScreen(this, "BrowserItem");
           screen.getURL("http://www.digg.com/rss/containerscience.xml");
           screen.addCommand( this.cmdBack );
           screen.setCommandListener( this );
-          display.setCurrent( screen );
+          this.display.setCurrent( screen );
           this.browserScreen = screen;
      }
 
@@ -48,4 +60,23 @@ public class BrowserMidlet
 		
 	}
 
+	public void commandAction(Command command, Item item)
+	{
+		if (command == RssTagHandler.CMD_RSS_ITEM_SELECT) {
+			RssItem rssItem = (RssItem) item.getAttribute(RssTagHandler.ATTR_RSS_ITEM);
+
+			if (rssItem != null) {
+				//#style rssItemView
+				Alert alert = new Alert( rssItem.getTitle(), rssItem.getDescription(), null, AlertType.ERROR );
+				alert.setTimeout(Alert.FOREVER);
+				this.display.setCurrent(alert);
+			}
+		}
+		else if (command == HtmlTagHandler.CMD_LINK) {
+			this.browserScreen.handleCommand(command);
+		}
+		else {
+			System.err.println("Unhandled command " + command);
+		}
+	}
 }
--- enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rssbrowser/BrowserScreen.java
+++ enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rssbrowser/BrowserScreen.java
@@ -1,39 +1,30 @@
 package de.enough.polish.sample.rssbrowser;
 
-import javax.microedition.lcdui.Alert;
-import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.Display;
 
 import de.enough.polish.browser.html.HtmlBrowser;
 import de.enough.polish.browser.rss.RssBrowser;
-import de.enough.polish.browser.rss.RssItem;
-import de.enough.polish.browser.rss.RssTagHandler;
 import de.enough.polish.ui.Form;
-import de.enough.polish.ui.Item;
 import de.enough.polish.ui.ItemCommandListener;
 import de.enough.polish.ui.Style;
 
 public class BrowserScreen
 	extends Form
-	implements ItemCommandListener
 {
-  private Display display;
   private HtmlBrowser htmlItem;
 
-  public BrowserScreen(Display display, String title)
+  public BrowserScreen(ItemCommandListener itemCommandListener, String title)
   {
-    this(display, title, null);
+    this(itemCommandListener, title, null);
   }
 
-  public BrowserScreen(Display display, String title, Style style)
+  public BrowserScreen(ItemCommandListener itemCommandListener, String title, Style style)
   {
     super(title, style);
 
-    this.display = display;
-
     //#style browser
-    this.htmlItem = new RssBrowser(this);
+    this.htmlItem = new RssBrowser(itemCommandListener);
     append(this.htmlItem);
   }
 
@@ -58,19 +49,5 @@ public class BrowserScreen
   public boolean handleCommand(Command command)
   {
     return this.htmlItem.handleCommand(command);
-  }
-
-  public void commandAction(Command command, Item item)
-  {
-	  if (command == RssTagHandler.CMD_RSS_ITEM_SELECT) {
-		  RssItem rssItem = (RssItem) item.getAttribute(RssTagHandler.RSS_ITEM);
-
-		  if (rssItem != null) {
-			  //#style rssItemView
-			  Alert alert = new Alert( rssItem.getTitle(), rssItem.getDescription(), null, AlertType.ERROR );
-			  alert.setTimeout(Alert.FOREVER);
-			  this.display.setCurrent(alert);
-		  }
-	  }
   }
 }
