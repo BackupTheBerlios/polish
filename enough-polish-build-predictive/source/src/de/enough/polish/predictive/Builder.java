d63c2f3c23d954421ecd0a0a6b97531d6455f397
infamous <infamous>
2007-07-06 14:41:41 +0000

    Now saves a .properties file named predictive.header for further use in the client

--- enough-polish-build-predictive/source/src/de/enough/polish/predictive/Builder.java
+++ enough-polish-build-predictive/source/src/de/enough/polish/predictive/Builder.java
@@ -7,6 +7,8 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.Vector;
 
 import org.apache.tools.ant.BuildException;
@@ -15,36 +17,52 @@ import org.apache.tools.ant.Task;
 public class Builder extends Task {
 	
 	private String source;
-	private String dest;
+	private String directory;
+	
+	private String version;
+	private String prefix;
+	private int chunkSize;
+	private int lineCount;
+	
+	
 	private Vector words = new Vector();
 	private BufferedReader reader;
-	private FileOutputStream writer;
-	ByteArrayOutputStream stream = new ByteArrayOutputStream( 0 );
+	private FileOutputStream trieWriter;
+	
+	ByteArrayOutputStream trieStream = null;
+	ByteArrayOutputStream headerStream = null;
 	
 	public void setSource(String source) {
 		this.source = source;
 	}
 	
-	public void setDest(String dest) {
-		this.dest = dest;
+	public void setDirectory(String directory) {
+		this.directory = directory;
 	}
 	
 	public void execute() throws BuildException {
 		try {
-			System.out.println("Reading " + source + "...");
+			System.out.println("Building " + this.directory + "/predictive.header ...");
+						
+			Properties properties = buildHeader();
+			properties.store(new FileOutputStream(directory + "/predictive.header"),"predictive.header");
+			
+			System.out.println("Reading " + this.source + "...");
 			
 			readWords();
-
-			System.out.println("Building " + dest + "...");
+			
+			System.out.println("Building " + this.directory + "/predictive.trie ...");
 			
 			Element base = new Element();
-						
+			trieStream = new ByteArrayOutputStream( 0 );
+			
 			buildTrie(base,0);
 			writeTrie(base);
 			
-			writer = new FileOutputStream(dest);
-			writer.write(stream.toByteArray());
-			writer.close();			
+			trieWriter = new FileOutputStream(directory + "/predictive.trie");
+			trieWriter.write(trieStream.toByteArray());
+			trieWriter.close();			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -64,6 +82,21 @@ public class Builder extends Task {
 		}
 	}
 	
+	public Properties buildHeader()
+	{
+		if(	this.version == null || this.prefix == null || this.chunkSize == 0 || this.lineCount == 0)
+			throw new BuildException("One or more of the header informations is set : please make sure to set version, prefix, chunkSize and lineCount");
+		
+		Properties properties = new Properties();
+		
+		properties.put("trie.version", this.version);
+		properties.put("trie.prefix", this.prefix);
+		properties.put("trie.chunkSize", "" + this.chunkSize);
+		properties.put("trie.lineCount", "" + this.lineCount);
+		
+		return properties;
+	}
+		
 	public int buildTrie(Element element, int recordID)
 	{
 		Vector children = element.findChildren(words);
@@ -89,21 +122,21 @@ public class Builder extends Task {
 		char referenceID = 0;
 		
 		if(children.size() > 0)
-			stream.write(byteToByte(children.size()));
+			trieStream.write(byteToByte(children.size()));
 		
 		for(int i=0; i < children.size(); i++)
 		{
 			Element child = (Element)children.elementAt(i);
 			
-			stream.write(charToByte(child.value));
-			stream.write(byteToByte(child.children.size()));
+			trieStream.write(charToByte(child.value));
+			trieStream.write(byteToByte(child.children.size()));
 			
 			if(child.children.size() > 0)
 				referenceID = (char)((Element)child.children.elementAt(0)).referenceID;
 			else
 				referenceID = (char)0;
 			
-			stream.write(charToByte(referenceID));
+			trieStream.write(charToByte(referenceID));
 		}
 		
 		for(int i=0; i < children.size(); i++)
@@ -131,5 +164,45 @@ public class Builder extends Task {
 	      dos.writeChar(character);
 	      dos.flush();
 	      return bos.toByteArray();
+	}
+
+	public int getChunkSize() {
+		return chunkSize;
+	}
+
+	public void setChunkSize(int chunkSize) {
+		this.chunkSize = chunkSize;
+	}
+
+	public int getLineCount() {
+		return lineCount;
+	}
+
+	public void setLineCount(int lineCount) {
+		this.lineCount = lineCount;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getDirectory() {
+		return directory;
+	}
+
+	public String getSource() {
+		return source;
 	}
 }
