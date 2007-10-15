5900d2fe67c8fb396a471d2b8e01a8872acb6a77
mkoch <mkoch>
2007-10-15 08:35:13 +0000

    - added copyright notice - removed unused variable

--- build/source/extensions/de/enough/polish/postcompile/java5/CloneMethodVisitor.java
+++ build/source/extensions/de/enough/polish/postcompile/java5/CloneMethodVisitor.java
@@ -1,3 +1,28 @@
+/*
+ * Created on Aug 16, 2006 at 12:51:15 PM.
+ * 
+ * Copyright (c) 2007 Michael Koch / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with J2ME Polish; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
 package de.enough.polish.postcompile.java5;
 
 import org.objectweb.asm.MethodVisitor;
@@ -18,15 +43,12 @@ public class CloneMethodVisitor
   private static final Method METHOD_CLONE =
     Method.getMethod("java.lang.Object clone()");
 
-  private Type enumType;
   private Type enumArrayDesc;
-  
   
   public CloneMethodVisitor(MethodVisitor mv, int access, String name, String desc, Type enumType)
   {
     super(mv, access, name, desc);
     
-    this.enumType = enumType;
     this.enumArrayDesc = Type.getType("[" + enumType.toString());
   }
 
