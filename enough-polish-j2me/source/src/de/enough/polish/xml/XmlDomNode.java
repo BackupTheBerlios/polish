a5e580ac4784cb852a03713a3cd20aa8d631a1fb
mkoch <mkoch>
2007-11-01 15:20:45 +0000

    - add XmlDomParser

---
+++ enough-polish-j2me/source/src/de/enough/polish/xml/XmlDomNode.java
@@ -0,0 +1,172 @@
+/*
+ * Created on 08-Apr-2006 at 19:20:28.
+ * 
+ * Copyright (c) 2006 Richard Nkrumah / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with J2ME Polish; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
+package de.enough.polish.xml;
+
+import de.enough.polish.util.ArrayList;
+
+/**
+ * <p>Copyright Enough Software 2006, 2007</p>
+ * 
+ * <pre>
+ * history
+ *        Apr 8, 2006 - rickyn creation
+ * </pre>
+ * 
+ * @author Richard Nkrumah, Richard.Nkrumah@enough.de
+ */
+public class XmlDomNode
+{
+    private XmlDomNode parent;
+    private ArrayList childList;
+    private String name;
+    private int type;
+    private String text;
+    
+    /**
+     * 
+     * @param parent
+     * @param name
+     * @param type
+     */
+    public XmlDomNode(XmlDomNode parent,String name,int type) {
+        this.parent = parent;
+        if(this.parent != null) {
+            this.parent.addChild(this);
+        }
+        this.name = name;
+        this.type = type;
+        this.childList = new ArrayList(); 
+    }
+    
+    /**
+     * 
+     * @param childName
+     * @return the first child with the given name or null if no child node is found.
+     */
+    public XmlDomNode getChild(String childName) {
+        XmlDomNode child;
+        for(int i = 0; i < this.childList.size();i++) {
+            child = (XmlDomNode)this.childList.get(i);
+            if(childName.equals(child.getName())) {
+                return child;
+            }
+        }
+        childName = childName.toLowerCase();
+        for(int i = 0; i < this.childList.size();i++) {
+            child = (XmlDomNode)this.childList.get(i);
+            if(childName.equals(child.getName())) {
+                return child;
+            }
+        }
+        return null;
+    }
+   
+    /**
+     * 
+     * @param index the child which has selected
+     * @return the value of the child
+     */
+    public String getChildText( int index ) {
+    	XmlDomNode child = (XmlDomNode) this.childList.get( index );
+    	if (child == null) {
+    		return null;
+    	} else {
+    		return child.text;
+    	}
+    }
+    
+    /**
+     * 
+     * @param childName
+     * @return the value of the child
+     */
+    public String getChildText( String childName ) {
+    	XmlDomNode child = getChild( childName );
+    	if (child == null) {
+    		return null;
+    	} else {
+    		return child.text;
+    	}
+    }
+
+    /**
+     * 
+     * @param i 
+     * @return Node
+     */
+    public XmlDomNode getChild(int i) {
+        return (XmlDomNode) this.childList.get(i);
+    }
+    
+    /**
+     * Add Node (child).
+     * 
+     * @param childNode
+     */
+    public void addChild(XmlDomNode childNode) {
+        this.childList.add(childNode);
+    }
+    
+    /**
+     * 
+     * @return number of Childs.
+     */
+    public int getChildCount() {
+        return this.childList.size();
+    }
+    
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public XmlDomNode getParent() {
+        return this.parent;
+    }
+    
+    public void setParent(XmlDomNode parent) {
+        this.parent = parent;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+    
+    public void setType(int type) {
+        this.type = type;
+    }
+}
---
+++ enough-polish-j2me/source/src/de/enough/polish/xml/XmlDomParser.java
@@ -0,0 +1,89 @@
+/*
+ * Created on 08-Apr-2006 at 19:20:28.
+ * 
+ * Copyright (c) 2006 Michael Koch / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with J2ME Polish; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
+package de.enough.polish.xml;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <p>Copyright Enough Software 2006, 2007</p>
+ * 
+ * <pre>
+ * history
+ *        Apr 8, 2006 - mkoch creation
+ * </pre>
+ * 
+ * @author Michael Koch, michael.koch@enough.de
+ */
+public class XmlDomParser
+{
+    public static XmlDomNode parseTree(String document)
+    {
+    	XmlPullParser parser;
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(document.getBytes());
+        InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream);
+
+        try {
+            parser = new XmlPullParser(inputStreamReader);
+        } catch (IOException exception) {
+            throw new RuntimeException("Could not create xml parser."+exception);
+        }
+
+        XmlDomNode root = new XmlDomNode(null, null, -1);
+        XmlDomNode currentNode = root;
+        int depth = -1;
+        String newName;
+        int newType;
+        
+        try {
+            while ((parser.next()) != XmlPullParser.END_DOCUMENT) {
+                newName = parser.getName();
+                newType = parser.getType();
+                
+                if(newType == XmlPullParser.START_TAG) {
+                    depth++;
+                    XmlDomNode newNode = new XmlDomNode(currentNode,newName,newType);
+                    currentNode = newNode;
+                }
+                
+                else if(newType == XmlPullParser.END_TAG) {
+                    depth--;
+                    currentNode = currentNode.getParent();
+                }
+                
+                else if(newType == XmlPullParser.TEXT) {
+                    String text = parser.getText();
+                    currentNode.setText(text);
+                }
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException("Could not parse tree:"+exception);
+        }
+        
+        return root;
+    }
+}
