a80ae4ee432cbbf2f48119a0615d42777f19c4c7
infamous <infamous>
2007-12-10 18:42:35 +0000

    - Renamed RagFile to RagStream - Added comments and javadoc

--- enough-polish-j2me/source/src/de/enough/polish/rag/RagContainer.java
+++ enough-polish-j2me/source/src/de/enough/polish/rag/RagContainer.java
@@ -1,3 +1,4 @@
+//#condition polish.usePolishGui && polish.useRAG
 package de.enough.polish.rag;
 
 import java.io.InputStream;
@@ -5,26 +6,63 @@ import java.io.InputStream;
 import de.enough.polish.io.Serializable;
 import de.enough.polish.util.ArrayList;
 
+/**
+ * <p>Stores a byte array with name and size to store or load from a RAG file</p>
+ *
+ * <p>Copyright (c) 2005, 2006, 2007 Enough Software</p>
+ * <pre>
+ * history
+ *        10-Dec-2007 - asc creation
+ * </pre>
+ * @author Andre Schmidt, andre@enough.de
+ */
+
 public class RagContainer implements Serializable{
 	private String name;
 	private long size;
 	private byte[] data;
 	
+	/**
+	 * @return the byte array
+	 */
 	public byte[] getData() {
 		return this.data;
 	}
+	
+	/**
+	 * Set the byte array
+	 * @param data the byte array
+	 */	
 	public void setData(byte[] data) {
 		this.data = data;
 	}
+	
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * Sets the name
+	 * @param name the name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * @return the size
+	 */
 	public long getSize() {
 		return this.size;
 	}
+	
+	/**
+	 * Sets the size. Must be the length of the byte array.
+	 * @param size the size
+	 */
 	public void setSize(long size) {
 		this.size = size;
 	}
