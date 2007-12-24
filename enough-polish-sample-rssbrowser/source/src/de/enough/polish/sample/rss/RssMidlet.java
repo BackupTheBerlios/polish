cc31d89a066182e6e9f9da60e88dfe926ee79528
enough <enough>
2007-12-24 15:02:15 +0000

    updated rss sample app

--- enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rss/RssMidlet.java
+++ enough-polish-sample-rssbrowser/source/src/de/enough/polish/sample/rss/RssMidlet.java
@@ -1,14 +1,4 @@
-package de.enough.polish.sample.rssbrowser;
-
-import de.enough.polish.browser.html.HtmlTagHandler;
-import de.enough.polish.browser.rss.RssBrowser;
-import de.enough.polish.browser.rss.RssItem;
-import de.enough.polish.browser.rss.RssTagHandler;
-import de.enough.polish.ui.Item;
-import de.enough.polish.ui.ItemCommandListener;
-import de.enough.polish.ui.UiAccess;
-import de.enough.polish.ui.splash.ApplicationInitializer;
-import de.enough.polish.ui.splash.InitializerSplashScreen;
+package de.enough.polish.sample.rss;
 
 import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.Choice;
@@ -23,16 +13,33 @@ import javax.microedition.lcdui.TextField;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
-public class BrowserMidlet 
+import de.enough.polish.browser.rss.RssBrowser;
+import de.enough.polish.browser.rss.RssItem;
+import de.enough.polish.browser.rss.RssTagHandler;
+import de.enough.polish.event.ThreadedCommandListener;
+import de.enough.polish.ui.Item;
+import de.enough.polish.ui.ItemCommandListener;
+import de.enough.polish.ui.UiAccess;
+import de.enough.polish.ui.splash.ApplicationInitializer;
+import de.enough.polish.ui.splash.InitializerSplashScreen;
+
+/**
+ * <p>Shows RSS news feeds</p>
+ *
+ * <p>Copyright Enough Software 2007</p>
+ * @author Robert Virkus, j2mepolish@enough.de
+ */
+public class RssMidlet 
 	extends MIDlet
 	implements CommandListener, ItemCommandListener, ApplicationInitializer
 {
-	private static final Command CMD_BACK = new Command("Back", Command.SCREEN, 5 );
+	private static final Command CMD_BACK = new Command("Back", Command.BACK, 5 );
 	private static final Command CMD_EXIT = new Command("Exit", Command.EXIT, 10);
 	private static final Command CMD_GO = new Command("Go", Command.OK, 2 );
 
 	private Display display;
-	private BrowserScreen browserScreen;
+	private Form browserScreen;
+	private RssBrowser rssBrowser;
 	private List mainMenu;
 	private String defaultRssUrl = "http://www.digg.com/rss/containerscience.xml";
 	private Form settingsForm;
@@ -53,6 +60,41 @@ public class BrowserMidlet
         	  this.display.setCurrent( disp );
           }
      }
+     
+
+ 	/* (non-Javadoc)
+ 	 * @see de.enough.polish.ui.splash.ApplicationInitializer#initApp()
+ 	 */
+ 	public Displayable initApp() {
+ 		 //#style myList
+         List menu = new List("RSS Menu", Choice.IMPLICIT);
+         //#style myChoiceItem
+         menu.append("Quick Launch", null);
+         //#style myChoiceItem
+         menu.append("Go to URL", null);
+         //#style myChoiceItem
+         menu.append("About", null);
+         //#style myChoiceItem
+         menu.append("Exit", null);
+         menu.addCommand( CMD_EXIT );
+         menu.setCommandListener(this);
+         this.mainMenu = menu;
+         
+         //#style rssBrowserForm
+         this.browserScreen = new Form("Browser");
+         //#style rssBrowser
+         this.rssBrowser = new RssBrowser( this );
+         this.browserScreen.append( this.rssBrowser );
+         this.browserScreen.addCommand(CMD_BACK);
+         this.browserScreen.addCommand(CMD_EXIT);
+         this.browserScreen.setCommandListener( new ThreadedCommandListener(this) );
+         
+         try { Thread.sleep(500); } catch (Exception e) { // just wait a little bit so that the splash screen can be recognized 
+        	 // ignore
+         }
+         
+ 		return menu;
+ 	}
 
      protected void pauseApp(){
           // ignore
@@ -63,7 +105,7 @@ public class BrowserMidlet
      }
 
 	public void commandAction(Command command, Displayable displayable) {
-		// #debug
+		//#debug
 		System.out.println("commandAction: cmd=" + command.getLabel() );
 		if (displayable == this.mainMenu) {
 			if (command == CMD_EXIT) {
@@ -86,22 +128,18 @@ public class BrowserMidlet
 				}
 			}
 		} else if (displayable == this.browserScreen){
-			if (this.browserScreen.processCommand(command)) {
+			if (this.rssBrowser.handleCommand(command)) {
 				return;
 			}
 	
 			if (command == CMD_BACK) {
-				if (this.browserScreen.canGoBack()) {
-					this.browserScreen.goBack();
+				if (this.rssBrowser.canGoBack()) {
+					this.rssBrowser.goBack();
 				} else {
 					this.display.setCurrent( this.mainMenu );
 				}
 			} else if (command == RssTagHandler.CMD_RSS_ITEM_SELECT || command.getLabel().equals("Select")) {
-				Item item = UiAccess.getFocusedItem(this.browserScreen);
-				if ( ((Object)item) instanceof RssBrowser) {
-					RssBrowser browser =(RssBrowser)((Object)item);
-					item = browser.getFocusedItem();
-				}
+				Item item = this.rssBrowser.getFocusedItem();
 				RssItem rssItem = (RssItem) item.getAttribute(RssTagHandler.ATTR_RSS_ITEM);
 				showRssNewsItem( rssItem );
 			}
@@ -124,13 +162,11 @@ public class BrowserMidlet
 	
 	public void commandAction(Command command, Item item)
 	{
+		//#debug
 		System.out.println("commandAction: cmd=" + command.getLabel() + " for item " + item);
 		if (command == RssTagHandler.CMD_RSS_ITEM_SELECT) {
 			RssItem rssItem = (RssItem) item.getAttribute(RssTagHandler.ATTR_RSS_ITEM);
 			showRssNewsItem( rssItem );
-		}
-		else if (command == HtmlTagHandler.CMD_LINK) {
-			this.browserScreen.processCommand(command);
 		}
 		else {
 			//#debug warn
@@ -149,7 +185,7 @@ public class BrowserMidlet
 	}
 
 	/**
-	 * 
+	 * Shows the about screen
 	 */
 	private void showAbout() {
 		//#style aboutAlert
@@ -166,7 +202,7 @@ public class BrowserMidlet
 	}
 
 	/**
-	 * 
+	 * Shows the settings screen
 	 */
 	private void showSettings() {
 		if (this.settingsForm == null) {
@@ -184,10 +220,10 @@ public class BrowserMidlet
 	}
 
 	/**
-	 * @param defaultRssUrl2
+	 * @param url the URL that should be shown
 	 */
 	private void showRssBrowser(String url) {
-		this.browserScreen.getURL(url);
+		this.rssBrowser.go(url);
 		String title = url.substring( "http://".length() );
 		//#style rssBrowserTitle
 		this.browserScreen.setTitle( title );
@@ -195,7 +231,7 @@ public class BrowserMidlet
 	}
 
 	/**
-	 * @return
+	 * @return the URL of the default RSS feed
 	 */
 	private String getDefaultRssUrl() {
 		return this.defaultRssUrl ;
@@ -203,33 +239,4 @@ public class BrowserMidlet
 
 	
 
-	/* (non-Javadoc)
-	 * @see de.enough.polish.ui.splash.ApplicationInitializer#initApp()
-	 */
-	public Displayable initApp() {
-		 //#style myList
-        List menu = new List("RSS Menu", Choice.IMPLICIT);
-        //#style myChoiceItem
-        menu.append("Quick Launch", null);
-        //#style myChoiceItem
-        menu.append("Go to URL", null);
-        //#style myChoiceItem
-        menu.append("About", null);
-        //#style myChoiceItem
-        menu.append("Exit", null);
-        menu.addCommand( CMD_EXIT );
-        menu.setCommandListener(this);
-        this.mainMenu = menu;
-        
-        //#style rssBrowserForm
-        BrowserScreen screen = new BrowserScreen(this, null);
-        screen.addCommand(CMD_BACK);
-        screen.addCommand(CMD_EXIT);
-        screen.setCommandListener( this );
-        this.browserScreen = screen;
-        
-        try { Thread.sleep(1000); } catch (Exception e) { }
-        
-		return menu;
-	}
 }
