807da1d8ef4ad0e30b61f54050c38c5edcb51984
infamous <infamous>
2008-01-31 18:30:18 +0000

    Changes for better memory management

--- enough-polish-j2me/source/src/de/enough/polish/multimedia/AudioPlayer.java
+++ enough-polish-j2me/source/src/de/enough/polish/multimedia/AudioPlayer.java
@@ -36,43 +36,52 @@ import javax.microedition.media.Player;
 import javax.microedition.media.PlayerListener;
 
 /**
- * <p>Plays back audio files - at the moment this is only supported for MIDP 2.0 and devices that support the MMAPI</p>
- *
- * <p>Copyright Enough Software 2006, 2007 - 2008</p>
+ * <p>
+ * Plays back audio files - at the moment this is only supported for MIDP 2.0
+ * and devices that support the MMAPI
+ * </p>
+ * 
+ * <p>
+ * Copyright Enough Software 2006, 2007 - 2008
+ * </p>
+ * 
  * <pre>
  * history
  *        Nov 21, 2006 - rob creation
  * </pre>
+ * 
  * @author Robert Virkus, j2mepolish@enough.de
  */
-public class AudioPlayer
-implements PlayerListener
-{
-	
+public class AudioPlayer implements PlayerListener {
+
 	private final static Hashtable AUDIO_TYPES = new Hashtable();
+
 	private final boolean doCachePlayer;
+
 	private Player player;
+
 	private PlayerListener listener;
+
 	private final String defaultContentType;
-	
+
 	public AudioPlayer() {
-		this( false, null, null );
-	}
-	
-	public AudioPlayer( boolean doCachePlayer ) {
-		this( doCachePlayer, null, null );
-	}
-	
-	public AudioPlayer( String contentType ) {
-		this( false, contentType, null );
-	}
-	
-	public AudioPlayer( boolean doCachePlayer, String contentType ) {
-		this( doCachePlayer, contentType, null );
+		this(false, null, null);
 	}
 
-	
-	public AudioPlayer( boolean doCachePlayer, String contentType, PlayerListener listener ) {
+	public AudioPlayer(boolean doCachePlayer) {
+		this(doCachePlayer, null, null);
+	}
+
+	public AudioPlayer(String contentType) {
+		this(false, contentType, null);
+	}
+
+	public AudioPlayer(boolean doCachePlayer, String contentType) {
+		this(doCachePlayer, contentType, null);
+	}
+
+	public AudioPlayer(boolean doCachePlayer, String contentType,
+			PlayerListener listener) {
 		this.listener = listener;
 		this.doCachePlayer = doCachePlayer;
 		if (contentType != null) {
@@ -87,20 +96,22 @@ implements PlayerListener
 		this.defaultContentType = contentType;
 	}
 
-	
-	public void play( String url, String type ) throws MediaException, IOException {
+	public void play(String url, String type) throws MediaException,
+			IOException {
 		InputStream in = getClass().getResourceAsStream(url);
 		if (in == null) {
-			throw new IOException("not found: " + url );
+			throw new IOException("not found: " + url);
 		}
-		play( in, type );
+		play(in, type);
 	}
-	
-	public void play( InputStream in, String type ) throws MediaException, IOException {
+
+	public void play(InputStream in, String type) throws MediaException,
+			IOException {
 		String correctType = getAudioType(type, "file");
 		if (correctType == null) {
 			//#debug warn
-			System.out.println("Unable to find correct type for " + type + " with the file protocol");
+			System.out.println("Unable to find correct type for " + type
+					+ " with the file protocol");
 			correctType = getAudioType(type, null);
 			if (correctType == null) {
 				//#debug warn
@@ -109,31 +120,31 @@ implements PlayerListener
 			}
 		}
 		this.player = Manager.createPlayer(in, correctType);
-		this.player.addPlayerListener( this );
-		this.player.start();
-	}
-	
-	
-	public void play( String url ) throws MediaException, IOException {
-		InputStream in = getClass().getResourceAsStream(url);
-		if (in == null) {
-			throw new IOException("not found: " + url );
-		}
-		play( in );
-	}
-	
-	public void play( InputStream in ) throws MediaException, IOException {
-		String correctType = this.defaultContentType;
-		this.player = Manager.createPlayer(in, correctType);
-		this.player.addPlayerListener( this );
+		this.player.addPlayerListener(this);
 		this.player.start();
 	}
 
-	
+	public void play(String url) throws MediaException, IOException {
+		InputStream in = getClass().getResourceAsStream(url);
+		if (in == null) {
+			throw new IOException("not found: " + url);
+		}
+		play(in);
+	}
+
+	public void play(InputStream in) throws MediaException, IOException {
+		String correctType = this.defaultContentType;
+		this.player = Manager.createPlayer(in, correctType);
+		this.player.addPlayerListener(this);
+		this.player.start();
+	}
+
 	/**
-	 * Plays back the last media again. This can only be used when doCachePlayer is set to true in the constructor
+	 * Plays back the last media again. This can only be used when doCachePlayer
+	 * is set to true in the constructor
 	 * 
-	 * @throws MediaException when the player cannot be started
+	 * @throws MediaException
+	 *             when the player cannot be started
 	 * @see #AudioPlayer(boolean)
 	 * @see #AudioPlayer(boolean, String)
 	 * @see #AudioPlayer(boolean, String, PlayerListener)
@@ -144,40 +155,52 @@ implements PlayerListener
 			this.player.start();
 		}
 	}
-	
+
 	/**
 	 * Returns the original player.
 	 * 
-	 * @return the original player, this can be null when no audio has been played back so far.
+	 * @return the original player, this can be null when no audio has been
+	 *         played back so far.
 	 */
 	public Player getPlayer() {
 		return this.player;
 	}
-	
+
 	/**
 	 * Helper function for getting a supported media type.
-	 * @param type the type like "audio/mp3"
-	 * @param protocol the protocol, when null is given the content type will be returned for any protocol
-	 * @return the type supported by the device, for example "audio/mpeg3" - null when the given type is not supported by the device.
+	 * 
+	 * @param type
+	 *            the type like "audio/mp3"
+	 * @param protocol
+	 *            the protocol, when null is given the content type will be
+	 *            returned for any protocol
+	 * @return the type supported by the device, for example "audio/mpeg3" -
+	 *         null when the given type is not supported by the device.
 	 */
-	public static String getAudioType( String type, String protocol ) {
+	public static String getAudioType(String type, String protocol) {
 		if (AUDIO_TYPES.size() == 0) {
-			addTypes( new String[]{ "audio/3gpp", "audio/3gp" } );
-			addTypes( new String[]{ "audio/x-mp3", "audio/mp3", "audio/x-mp3", "audio/mpeg3", "audio/x-mpeg3", "audio/mpeg-3" } );
-			addTypes( new String[]{ "audio/midi", "audio/x-midi", "audio/mid", "audio/x-mid", "audio/sp-midi" } );
-			addTypes( new String[]{ "audio/wav", "audio/x-wav" } );
-			addTypes( new String[]{ "audio/amr", "audio/x-amr" } );
-			addTypes( new String[]{ "audio/mpeg4", "audio/mpeg-4", "audio/mp4", "audio/mp4a-latm" } );
-			addTypes( new String[]{ "audio/imelody", "audio/x-imelody", "audio/imy", "audio/x-imy" } );
+			addTypes(new String[] { "audio/3gpp", "audio/3gp" });
+			addTypes(new String[] { "audio/x-mp3", "audio/mp3", "audio/x-mp3",
+					"audio/mpeg3", "audio/x-mpeg3", "audio/mpeg-3" });
+			addTypes(new String[] { "audio/midi", "audio/x-midi", "audio/mid",
+					"audio/x-mid", "audio/sp-midi" });
+			addTypes(new String[] { "audio/wav", "audio/x-wav" });
+			addTypes(new String[] { "audio/amr", "audio/x-amr" });
+			addTypes(new String[] { "audio/mpeg4", "audio/mpeg-4", "audio/mp4",
+					"audio/mp4a-latm" });
+			addTypes(new String[] { "audio/imelody", "audio/x-imelody",
+					"audio/imy", "audio/x-imy" });
 		}
-		String[] supportedContentTypes = Manager.getSupportedContentTypes( protocol );
+		String[] supportedContentTypes = Manager
+				.getSupportedContentTypes(protocol);
 		if (supportedContentTypes == null || supportedContentTypes.length == 0) {
 			return null;
 		}
-		Hashtable mappings = (Hashtable) AUDIO_TYPES.get( type );
+		Hashtable mappings = (Hashtable) AUDIO_TYPES.get(type);
 		if (mappings == null) {
 			//#debug warn
-			System.out.println("The audio content type " + type + " has no known synonyms.");
+			System.out.println("The audio content type " + type
+					+ " has no known synonyms.");
 			for (int i = 0; i < supportedContentTypes.length; i++) {
 				String contentType = supportedContentTypes[i];
 				if (contentType.equals(type)) {
@@ -195,42 +218,46 @@ implements PlayerListener
 
 		return null;
 	}
-	
-	public boolean isPlaying()
-	{
-		if(this.player == null)
-		{
+
+	public boolean isPlaying() {
+		if (this.player == null) {
 			return false;
-		}
-		else
-		{
+		} else {
 			return this.player.getState() == Player.STARTED;
 		}
 	}
 
-	private static final void addTypes( String[] types ) {
+	private static final void addTypes(String[] types) {
 		Hashtable nestedMap = new Hashtable();
 		for (int i = 0; i < types.length; i++) {
 			String type = types[i];
-			nestedMap.put( type, type );
-			AUDIO_TYPES.put( type, nestedMap );			
+			nestedMap.put(type, type);
+			AUDIO_TYPES.put(type, nestedMap);
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see javax.microedition.media.PlayerListener#playerUpdate(javax.microedition.media.Player, java.lang.String, java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.microedition.media.PlayerListener#playerUpdate(javax.microedition.media.Player,
+	 *      java.lang.String, java.lang.Object)
 	 */
 	public void playerUpdate(Player p, String event, Object data) {
+
 		if (this.listener != null) {
 			this.listener.playerUpdate(p, event, data);
 		}
+
 		if (!this.doCachePlayer && PlayerListener.END_OF_MEDIA.equals(event)) {
-			p.removePlayerListener( this );
-			p.deallocate();
+			p.removePlayerListener(this);
+			cleanUpPlayer();
+		}
+	}
+
+	public void cleanUpPlayer() {
+		if (this.player != null) {
+			player.deallocate();
 			this.player = null;
 		}
 	}
-	
-	
-
 }
