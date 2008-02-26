c8e304a9fceffee9c27d286d222d187f82de78a7
enough <enough>
2008-02-26 15:39:23 +0000

    simplified font creation

--- build/source/src/de/enough/polish/preprocess/css/FontConverter.java
+++ build/source/src/de/enough/polish/preprocess/css/FontConverter.java
@@ -25,9 +25,6 @@
  */
 package de.enough.polish.preprocess.css;
 
-import de.enough.polish.BuildException;
-
-import java.util.HashMap;
 
 /**
  * <p>Respresents a J2ME font.</p>
@@ -41,37 +38,11 @@ import java.util.HashMap;
  * @author Robert Virkus, robert@enough.de
  */
 public class FontConverter extends Converter {
-	private final static HashMap STYLES = new HashMap();
-	static {
-		STYLES.put( "plain", "STYLE_PLAIN" );
-		STYLES.put( "default", "STYLE_PLAIN" );
-		STYLES.put( "normal", "STYLE_PLAIN" );
-		STYLES.put( "bold", "STYLE_BOLD" );
-		STYLES.put( "italic", "STYLE_ITALIC" );
-		STYLES.put( "cursive", "STYLE_ITALIC" );
-		STYLES.put( "underlined", "STYLE_UNDERLINED" );
-	}
-	private final static HashMap SIZES = new HashMap();
-	static {
-		SIZES.put( "small", "SIZE_SMALL" );
-		SIZES.put( "medium", "SIZE_MEDIUM" );
-		SIZES.put( "default", "SIZE_MEDIUM" );
-		SIZES.put( "normal", "SIZE_MEDIUM" );
-		SIZES.put( "large", "SIZE_LARGE" );
-		SIZES.put( "big", "SIZE_LARGE" );
-	}
-	private final static HashMap FACES = new HashMap();
-	static {
-		FACES.put( "system", "FACE_SYSTEM");
-		FACES.put( "default", "FACE_SYSTEM");
-		FACES.put( "normal", "FACE_SYSTEM");
-		FACES.put( "monospace", "FACE_MONOSPACE");
-		FACES.put( "proportional", "FACE_PROPORTIONAL");
-	}
+
 	
-	private String face = "FACE_SYSTEM";
+	private String face  = "Font.FACE_SYSTEM";
 	private String style = "Font.STYLE_PLAIN";
-	private String size = "SIZE_MEDIUM";
+	private String size  = "Font.SIZE_MEDIUM";
 	
 	/**
 	 * Creates a new font converter.
@@ -89,9 +60,6 @@ public class FontConverter extends Converter {
 	public String createNewStatement() {
 		StringBuffer fontCode = new StringBuffer();
 		
-		
-			
-			
 		// first param is the font face:
 		fontCode.append("Font.getFont(Font.")
 				.append( this.face )
@@ -108,33 +76,20 @@ public class FontConverter extends Converter {
 	 * @param face The face to set.
 	 */
 	public void setFace(String face) {
-		face = face.toLowerCase();
-		this.face = (String) FACES.get( face );
-		if (this.face == null) {
-			throw new BuildException("Invalid CSS: the font-face [" + face + "] is not supported. Allowed values are [system], [proportional] and [monospace].");
-		}
+		this.face = face;
 	}
 	
 	/**
 	 * @param size The size to set.
 	 */
 	public void setSize(String size) {
-		size = size.toLowerCase();
-		this.size = (String) SIZES.get( size );
-		if (this.size == null) {
-			throw new BuildException("Invalid CSS: the font-size [" + size + "] is not supported. Allowed values are [small], [medium] and [large].");
-		}
+		this.size = size;
 	}
 	/**
 	 * @param style The style to set.
 	 */
 	public void setStyle(String style) {
 		this.style = style;
-//		style = style.toLowerCase();
-//		this.style = (String) STYLES.get( style );
-//		if (this.style == null) {
-//			throw new BuildException("Invalid CSS: the font-style [" + style + "] is not supported. Allowed values are [plain], [bold], [italic] and [underlined].");
-//		}
 	}
 
 }
