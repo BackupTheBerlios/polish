cb9c2157b82b55602e775115b2d726a347f1b649
enough <enough>
2008-04-10 00:03:11 +0000

    improved architecture

--- enough-polish-sample-browser/source/src/de/enough/polish/sample/browser/BrowserMidlet.java
+++ enough-polish-sample-browser/source/src/de/enough/polish/sample/browser/BrowserMidlet.java
@@ -4,9 +4,18 @@ import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
+import de.enough.polish.browser.html.HtmlBrowser;
+
+/**
+ * <p>Demonstrates a simple browser app using the HtmlBrowser component of J2ME Polish.</p>
+ *
+ * <p>Copyright Enough Software 2008</p>
+ * @author Robert Virkus, j2mepolish@enough.de
+ */
 public class BrowserMidlet 
 extends MIDlet
 implements CommandListener
@@ -14,18 +23,23 @@ implements CommandListener
 	
 	private Command cmdBack = new Command("Back", Command.BACK, 9);
 	private Command cmdExit = new Command("Exit", Command.EXIT, 10 );
-	private BrowserScreen browserScreen;
+	private Form browserScreen;
+	private HtmlBrowser htmlBrowser;
 
      protected void startApp() throws MIDletStateChangeException{
           Display display = Display.getDisplay( this );
           if (this.browserScreen == null) {
 	          //#style browserScreen
-	          BrowserScreen screen = new BrowserScreen("HTML Browser");
-	          screen.go( "resource://index.html");
-	          screen.addCommand( this.cmdBack );
-	          screen.addCommand( this.cmdExit );
-	          screen.setCommandListener( this );
-	          this.browserScreen = screen;
+	          Form form = new Form("HTML Browser");
+	          //#style browser
+	          this.htmlBrowser = new HtmlBrowser();
+	          new ChartTagHandler( this.htmlBrowser.getTagHandler("div")).register(this.htmlBrowser);
+	          form.append(this.htmlBrowser);
+	          this.htmlBrowser.go( "resource://index.html");
+	          this.htmlBrowser.setBackCommand( this.cmdBack );
+	          form.addCommand( this.cmdExit );
+	          form.setCommandListener( this );
+	          this.browserScreen = form;
           }
           display.setCurrent( this.browserScreen );
      }
@@ -42,7 +56,7 @@ implements CommandListener
 	    //#debug
 		System.out.println("BrowserMidlet.commandAction for cmd=" + cmd.getLabel() );
 		if (cmd == this.cmdBack) {
-			this.browserScreen.goBack();
+			this.htmlBrowser.goBack();
 		} else if (cmd == this.cmdExit) {
 			//destroyApp( true );
 			notifyDestroyed();
