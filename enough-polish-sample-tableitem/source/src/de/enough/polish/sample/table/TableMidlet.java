805bc7bda2737779a12380a23b15cada2d653b26
enough <enough>
2008-11-26 16:13:53 +0000

    updated table item sample

--- enough-polish-sample-tableitem/source/src/de/enough/polish/sample/table/TableMidlet.java
+++ enough-polish-sample-tableitem/source/src/de/enough/polish/sample/table/TableMidlet.java
@@ -2,6 +2,7 @@ package de.enough.polish.sample.table;
 
 import java.io.IOException;
 
+import javax.microedition.lcdui.Alert;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
@@ -12,11 +13,7 @@ import javax.microedition.lcdui.ImageItem;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
-import de.enough.polish.ui.ChartItem;
-import de.enough.polish.ui.Style;
 import de.enough.polish.ui.TableItem;
-import de.enough.polish.ui.UiAccess;
-import de.enough.polish.ui.backgrounds.SimpleBackground;
 import de.enough.polish.util.TableData;
 
 public class TableMidlet 
@@ -25,26 +22,29 @@ implements CommandListener
 {
 	
 	private Form form;
-	private Command exitCommand = new Command( "Exit", Command.EXIT, 3 );
+	private Command cmdExit = new Command( "Exit", Command.EXIT, 3 );
+	private Command cmdShowCountry = new Command("Show", Command.OK, 1 );
+	private TableItem tableCountries;
+	private Display display;
 
 	public TableMidlet() {
 		//#style mainScreen
 		this.form = new Form( "J2ME Polish Table");
-		this.form.addCommand( this.exitCommand );
+		this.form.addCommand( this.cmdExit );
 		this.form.setCommandListener( this );
 
 
 	}
 
      protected void startApp() throws MIDletStateChangeException{
-          Display display = Display.getDisplay( this );
+          this.display = Display.getDisplay( this );
           try {
-          updateTable();
+        	  updateTables();
           } catch (Exception e) {
         	  //#debug error
         	  System.out.println("Unable to initialize table completely");
           }
-          display.setCurrent( this.form );
+          this.display.setCurrent( this.form );
      }
 
      protected void pauseApp(){
@@ -59,12 +59,17 @@ implements CommandListener
 	 * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
 	 */
 	public void commandAction(Command cmd, Displayable disp) {
-		if (cmd == this.exitCommand) {
+		if (cmd == this.cmdExit) {
 			notifyDestroyed();
+		} else if (cmd == this.cmdShowCountry) {
+			ImageItem item = (ImageItem) this.tableCountries.getSelectedCell();
+			//#style countryAlert
+			Alert alert = new Alert( item.getAltText() );
+			this.display.setCurrent( alert );
 		}
 	}
 	
-	private void updateTable() throws IOException{
+	private void updateTables() throws IOException{
 		this.form.deleteAll();
 		//#style label
 		this.form.append("without cell design:");
@@ -121,15 +126,18 @@ implements CommandListener
 		table.set( 0, 1, "Country:");
 		Image img = Image.createImage("/fi.png");
 		//#style centeredCell
-		ImageItem imgItem = new ImageItem(null, img, ImageItem.PLAIN, null);
+		ImageItem imgItem = new ImageItem(null, img, ImageItem.PLAIN, "Finland");
+		imgItem.setDefaultCommand( this.cmdShowCountry );
 		table.set( 1, 1, imgItem);
 		img = Image.createImage("/ko.png");
 		//#style centeredCell
-		imgItem = new ImageItem(null, img, ImageItem.PLAIN, null);
+		imgItem = new ImageItem(null, img, ImageItem.PLAIN, "Korea");
+		imgItem.setDefaultCommand( this.cmdShowCountry );
 		table.set( 2, 1, imgItem);
 		img = Image.createImage("/us.png");
 		//#style centeredCell
-		imgItem = new ImageItem(null, img, ImageItem.PLAIN, null);
+		imgItem = new ImageItem(null, img, ImageItem.PLAIN, "USA");
+		imgItem.setDefaultCommand( this.cmdShowCountry );
 		table.set( 3, 1, imgItem);
 		//#style heading
 		table.set( 0, 2, "Share:");
@@ -157,6 +165,7 @@ implements CommandListener
 		table.set( 3, 4, "21%");
 		
 		table.setSelectionMode( TableItem.SELECTION_MODE_ROW | TableItem.SELECTION_MODE_COLUMN );
+		this.tableCountries = table;
 		//table.setSelectedBackground( new SimpleBackground(0xffff00)); // using CSS instead now
 
 		this.form.append( table );
