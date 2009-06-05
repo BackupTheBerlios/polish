e2b4572d0f7f88c374a1c335d6ce71af118f10b8
mkoch <mkoch>
2009-06-05 07:21:21 +0000

    - fix output in main method - remove debug output

--- enough-polish-build/source/src/de/enough/polish/linuxnotify/LinuxNotifier.java
+++ enough-polish-build/source/src/de/enough/polish/linuxnotify/LinuxNotifier.java
@@ -17,7 +17,6 @@ public class LinuxNotifier {
 	public static boolean publish(String title, String text)
 	{
 		try {
-			System.err.println("Michael: publish");
 			String[] cmd = new String[] {"notify-send", "-t", "0", title, text };
 			Runtime.getRuntime().exec(cmd);
 			return true;
@@ -34,7 +33,6 @@ public class LinuxNotifier {
 	 */
 	public static boolean isNotifyAvailable() {
 		String oSName = System.getProperty("os.name").toLowerCase();
-		System.err.println("Michael: isAvailable: " + oSName);
 		boolean isLinux = oSName.startsWith("linux");
 		//TODO check if 'notify-send' command is really available. 
 		return isLinux;
@@ -43,7 +41,7 @@ public class LinuxNotifier {
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("usage:");
-			System.out.println("java de.enough.polish.growl.GrowlNotifier [title] [description]");
+			System.out.println("java de.enough.polish.linuxnotify.LinuxNotifier [title] [description]");
 			System.exit(1);
 		}
 		if (isNotifyAvailable()) {
