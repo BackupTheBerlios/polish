33eab2d74a0bb53fe79a5759a0e8d15e94c419bb
rickyn <rickyn>
2009-07-06 13:52:28 +0000

    status handling simplyfied

--- j2me/source/src/de/enough/polish/android/location/AndroidLocationListenerAdapter.java
+++ j2me/source/src/de/enough/polish/android/location/AndroidLocationListenerAdapter.java
@@ -1,10 +1,8 @@
 //#condition polish.android
 package de.enough.polish.android.location;
 
-import de.enough.polish.util.DeviceControl;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 
 public class AndroidLocationListenerAdapter implements LocationListener {
@@ -34,74 +32,46 @@ public class AndroidLocationListenerAdapter implements LocationListener {
 		this.listener.locationUpdated(this.locationProvider, wrappedLocation);
 	}
 
+	/**
+	 * Called when the user disables a location provider.
+	 */
 	public void onProviderDisabled(String providerName) {
 		//#debug
 		System.out.println("onProviderDisabled:"+providerName);
-		// When the GPS provider is disabled, switch to the NETWORK provider.
-		if( ! LocationManager.GPS_PROVIDER.equals(providerName)) {
-			//#debug
-			System.out.println("onProviderDisabled: The disabled location provider '"+providerName+"' is not the GPS provider. There is no need to do anything now.");
-			return;
-		}
-		if(DeviceControl.isFallbackToNetworkLocationOnGpsDisabled()) {
-			//#debug
-			System.out.println("onProviderDisabled: As a fallback, I set the NETWORK provider as new location provider.");
-			this.locationProvider.setProviderName(LocationManager.NETWORK_PROVIDER);
-		}
 	}
 
+	/**
+	 * Called when the user enables a location provider.
+	 */
 	public void onProviderEnabled(String providerName) {
 		//#debug
 		System.out.println("onProviderEnabled:"+providerName);
-		// The provider which was requested by criteria became available.
-		String providerByCritera = this.locationProvider.getProviderByCritera();
-		if( ! providerName.equals(providerByCritera)) {
-			//#debug
-			System.out.println("onProviderEnabled: The enabled location provider '"+providerName+"' is not the one requested by the criteria which was '"+providerByCritera+"'. Doing nothing about it.");
-			return;
-		}
-		//#debug
-		System.out.println("onProviderEnabled: Setting the location provider back to '"+providerByCritera+"' which is the one requested by the user critera.");
-		this.locationProvider.setProviderName(providerByCritera);
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		String statusName;
+		int translatedStatus;
 		switch(status) {
 			case android.location.LocationProvider.AVAILABLE:
 				statusName = "Available";
+				translatedStatus = LocationProvider.AVAILABLE;
 				break;
 			case android.location.LocationProvider.OUT_OF_SERVICE:
 				statusName = "Out of Service";
+				translatedStatus = LocationProvider.OUT_OF_SERVICE;
 				break;
 			case android.location.LocationProvider.TEMPORARILY_UNAVAILABLE:
 				statusName = "Temporarily Unavailable";
+				translatedStatus = LocationProvider.TEMPORARILY_UNAVAILABLE;
 				break;
 			default: throw new IllegalArgumentException("LocationProvider.onStatusChanged: Status '"+status+"' not recognized.");
 		}
 		
 		//#debug
 		System.out.println("onStatusChanged:provider:"+provider+".Status:"+statusName);
-		int translatedStatus;
-		switch(status) {
-			case android.location.LocationProvider.AVAILABLE:
-				translatedStatus = LocationProvider.AVAILABLE;
-				//TODO: Maybe these methods onProviderEnabled and onProviderDisabled must not be implemented as onSatusChange is sufficient.
-				onProviderEnabled(provider);
-				break;
-				
-			case android.location.LocationProvider.OUT_OF_SERVICE:
-				translatedStatus = LocationProvider.OUT_OF_SERVICE;
-				break;
-				
-			case android.location.LocationProvider.TEMPORARILY_UNAVAILABLE:
-				translatedStatus = LocationProvider.TEMPORARILY_UNAVAILABLE;
-				onProviderDisabled(provider);
-				break;
-				
-			default: throw new IllegalArgumentException("LocationProvider: Status change '"+status+"' not recognized.");
-		}
 		this.listener.providerStateChanged(this.locationProvider, translatedStatus);
+		
+		this.locationProvider.setState(translatedStatus);
 	}
 	
 	public void setListener(de.enough.polish.android.location.LocationListener locationListener) {
