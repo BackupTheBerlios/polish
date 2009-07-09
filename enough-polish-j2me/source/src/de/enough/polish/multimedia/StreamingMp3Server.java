08b69d08158805320f30eb37e2c029ef3d73cf50
rickyn <rickyn>
2009-07-09 15:33:28 +0000

    The server seems to work but the MediaPlayer is still not happy

--- enough-polish-j2me/source/src/de/enough/polish/multimedia/StreamingMp3Server.java
+++ enough-polish-j2me/source/src/de/enough/polish/multimedia/StreamingMp3Server.java
@@ -1,19 +1,19 @@
 //#condition polish.android
 package de.enough.polish.multimedia;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.URI;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 import java.util.Iterator;
 
 import de.enough.polish.util.TextUtil;
@@ -21,26 +21,28 @@ import de.enough.polish.util.TextUtil;
 public class StreamingMp3Server implements Runnable {
 
 	public static final int PORT_STREAMING_MP3 = 6754;
-	public static final String RESPONSE_HEADER = "HTTP/1.1 200 OK\r\nContent-Type: audio/mp3\r\n\r\n";
-	
-	private ByteBuffer responseHeader;
-	private CharsetDecoder decoder;
-	
-	public StreamingMp3Server() {
-		super();
-		byte[] bytes;
-		try {
-			bytes = RESPONSE_HEADER.getBytes("US-ASCII");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			// This is unlikely to happen.
-			return;
-		}
-		this.responseHeader = ByteBuffer.wrap(bytes);
-		Charset charset = Charset.forName("US-ASCII");
-		this.decoder = charset.newDecoder();
+	private static final String CRLF = "\r\n";
+	public static final String RESPONSE_HEADER_HEAD; 
+	public static final String RESPONSE_HEADER_GET;
+	static {
+		RESPONSE_HEADER_HEAD =
+			"HTTP/1.1 200 OK"+CRLF+
+			"Date: Wed, 08 Jul 2009 12:17:24 GMT"+CRLF+
+			"Last-Modified: Wed, 08 Jul 2009 12:10:22 GMT"+CRLF+
+			"Accept-Ranges: bytes"+CRLF+
+			"Connection: Keep-Alive"+CRLF+
+			"Content-Type: audio/mpeg"+CRLF+
+			CRLF;
+		RESPONSE_HEADER_GET =
+			"HTTP/1.1 200 OK"+CRLF+
+			"Date: Wed, 08 Jul 2009 12:17:24 GMT"+CRLF+
+			"Last-Modified: Wed, 08 Jul 2009 12:10:22 GMT"+CRLF+
+			"Accept-Ranges: bytes"+CRLF+
+			"Connection: close"+CRLF+
+			"Content-Type: audio/mpeg"+CRLF+
+			CRLF;
 	}
-
+	
 	public void start() throws IOException {
 		Thread thread = new Thread(this);
 		thread.start();
@@ -75,19 +77,12 @@ public class StreamingMp3Server implements Runnable {
 					it.remove();
 	
 					if (selectedKey.isAcceptable()) {
-	//					serverChannel = (ServerSocketChannel) selKey.channel();
 						SocketChannel channel = serverChannel.accept();
 						if (channel == null) {
 							// This case may happen as the key is not synchronized. Its possible that the channel of the key was closed
 							// since it was selected.
 							continue;
 						}
-						channel.configureBlocking(false);
-				        channel.register (selector, SelectionKey.OP_READ);
-					}
-					
-					if(selectedKey.isReadable()) {
-						SocketChannel channel = (SocketChannel)selectedKey.channel();
 						handleChannel(channel);
 					}
 				}
@@ -99,68 +94,87 @@ public class StreamingMp3Server implements Runnable {
 	}
 	
 	private void handleChannel(SocketChannel channel) throws IOException {
-//		Socket socket = channel.socket();
-//		InputStream inputStream = socket.getInputStream();
-//		InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"US-ASCII");
-//		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-//		String header = bufferedReader.readLine();
-		ByteBuffer byteBuffer = ByteBuffer.allocate(1024*8);
-		StringBuffer stringBuffer = new StringBuffer();
-		while(channel.read(byteBuffer) > 0) {
-//			CharBuffer charBuffer = this.decoder.decode(byteBuffer);
-			byte[] array = byteBuffer.array();
-			stringBuffer.append(new String(array));
-		}
-		String header = stringBuffer.toString();
+		Socket socket = channel.socket();
+		InputStream inputStream = socket.getInputStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"US-ASCII");
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader,1024*8);
+		String httpRequest = bufferedReader.readLine();
 		//#debug
-		System.out.println("header:"+header+"XXX");
-		int indexOf = header.indexOf("\r");
-		if(indexOf != -1) {
-			header = header.substring(0,indexOf);
-		} else {
-			header = header.substring(0,header.indexOf("\n"));
-		}
-		String[] parts = TextUtil.split(header, ' ');
+		System.out.println("header:"+httpRequest+"XXX");
+		String[] parts = TextUtil.split(httpRequest, ' ');
 		if("HEAD".equals(parts[0])) {
-			channel.write(this.responseHeader);
+			handleHead(channel, parts);
 		}
 		else if("GET".equals(parts[0])) {
-			String urlString = parts[1];
-			URI uri = URI.create(urlString);
-//			String path = uri.getPath();
-//			parts = TextUtil.split(path, ' ');
-			String query = uri.getQuery();
-			String[] fields = TextUtil.split(query, '&');
-			String[] filenames = new String[fields.length];
-			for (int i = 0; i < fields.length; i++) {
-				String field = fields[i];
-				filenames[i] = field.substring(field.indexOf('=')+1);
-			}
-			
-			channel.write(this.responseHeader);
-			
-			for (int i = 0; i < filenames.length; i++) {
-				String filename = filenames[i];
-				FileInputStream fileInputStream = new FileInputStream(filename);
-				FileChannel fileChannel = fileInputStream.getChannel();
-				long position = 0;
-				long length;
-				long count = fileChannel.size();
-				do {
-					length = fileChannel.transferTo(0, count, channel);
-					position = position + length;
-					count = count - length;
-				}
-				while(length > 0);
-				fileChannel.close();
-//				break;
-			}
+			handleGet(channel, parts);
 		}
 		else {
 			//#debug
 			System.err.println("Unknown request:"+parts[0]);
 		}
+		//#debug
+		System.out.println("Closing socket stream.");
+		//TODO: Put this call in a finally block.
 		channel.close();
+	}
+
+	private void handleHead(SocketChannel channel, String[] parts) throws IOException {
+		OutputStream outputStream = channel.socket().getOutputStream();
+		outputStream.write(RESPONSE_HEADER_HEAD.getBytes("US-ASCII"));
+		//#debug
+		System.out.println("Writing header for HEAD response to socket stream.");
+		outputStream.flush();
+	}
+
+	private void handleGet(SocketChannel channel, String[] parts) throws IOException {
+		String urlString = parts[1];
+		String[] filenames = extractFilenamesFromUrl(urlString);
+		byte[] dataBuffer = new byte[1024 * 8];
+		OutputStream outputStream = channel.socket().getOutputStream();
+		try {
+			outputStream.write(RESPONSE_HEADER_GET.getBytes("US-ASCII"));
+			outputStream.flush();
+			//#debug
+			System.out.println("Writing header with size '"+RESPONSE_HEADER_GET.length()+"' to socket stream.");
+			for (int i = 0; i < filenames.length; i++) {
+				String filename = filenames[i];
+				FileInputStream fileInputStream = new FileInputStream(filename);
+				int numberOfBytesRead = 1;
+				
+				// Write a file to the stream.
+				while(numberOfBytesRead != -1) {
+					numberOfBytesRead = fileInputStream.read(dataBuffer);
+					//#debug
+					System.out.println("Read '"+numberOfBytesRead+"' bytes from file.");
+					if(numberOfBytesRead <= 0) {
+						break;
+					}
+					//#debug
+					System.out.println("About to write data to socket stream.");
+					outputStream.write(dataBuffer,0,numberOfBytesRead);
+					outputStream.flush();
+					//#debug
+					System.out.println("Bytes written to socket stream.");
+				}
+				fileInputStream.close();
+			}
+		} finally {
+			outputStream.close();
+		}
 		
+	}
+
+	private String[] extractFilenamesFromUrl(String urlString) {
+		URI uri = URI.create(urlString);
+		String query = uri.getQuery();
+		String[] fields = TextUtil.split(query, '&');
+		String[] filenames = new String[fields.length];
+		for (int i = 0; i < fields.length; i++) {
+			String field = fields[i];
+			filenames[i] = field.substring(field.indexOf('=')+1);
+			//#debug
+			System.out.println("File in url found:"+filenames[i]);
+		}
+		return filenames;
 	}
 }
