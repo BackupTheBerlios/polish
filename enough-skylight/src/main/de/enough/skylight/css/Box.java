3177d99b717047af0a3e239773ed3247a6658efa
rickyn <rickyn>
2009-10-27 17:02:46 +0000

    First idea for the css apply and format

--- enough-skylight/src/main/de/enough/skylight/css/Box.java
+++ enough-skylight/src/main/de/enough/skylight/css/Box.java
@@ -4,7 +4,7 @@ import de.enough.skylight.dom.DomNode;
 
 
 /**
- * Objects represent boxes in the CSS model. A box contains CSS properties and a reference to its containing block.
+ * These objects represent boxes in the CSS model. A box contains CSS properties and a reference to its containing block.
  * Normally a DomNode should implement this interface
  * @author rickyn
  *
@@ -13,6 +13,14 @@ public interface Box {
 
 	public String setProperty(String propertyName, String computedValue);
 	public String getProperty(String propertyName);
+	/**
+	 * The box this box is contained in.
+	 * @return
+	 */
 	public Box getContainingBlock();
+	
 	public DomNode getNode();
+	public void addChild(Box child);
+	public void prependChild(Box referenceChild, Box childToPrepend);
+	public void appendChild(Box referenceChild, Box childToAppend);
 }
