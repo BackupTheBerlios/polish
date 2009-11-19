cdd77460e0f19219582a4186b1783a178ba2dc36
infamous <infamous>
2009-11-19 17:32:31 +0000

    *** empty log message ***

--- enough-polish-sample-skylight-api/src/de/enough/polish/skylight/builder/view/NodeUtils.java
+++ enough-polish-sample-skylight-api/src/de/enough/polish/skylight/builder/view/NodeUtils.java
@@ -1,13 +1,12 @@
 package de.enough.polish.skylight.builder.view;
 
-import de.enough.skylight.dom.Attr;
 import de.enough.skylight.dom.DomNode;
 import de.enough.skylight.dom.NamedNodeMap;
 
 public class NodeUtils {
 	public final static String getAttributeValue(DomNode node, String name) {
 		NamedNodeMap nodeMap = node.getAttributes();
-		Attr attribute = (Attr)nodeMap.getNamedItem(name); 
-		return attribute.getValue();
+		DomNode attribute = nodeMap.getNamedItem(name);
+		return attribute.getNodeValue();
 	}
 }
