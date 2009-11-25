36763579560918c19c8dbf2ed5396583dbe16cad
rickyn <rickyn>
2009-11-25 15:37:16 +0000

    another refactored js class

---
+++ enough-skylight/src/main/de/enough/skylight/dom/impl/rhino/EventScriptableObject.java
@@ -0,0 +1,13 @@
+package de.enough.skylight.dom.impl.rhino;
+
+import org.mozilla.javascript.ScriptableObject;
+
+public class EventScriptableObject extends ScriptableObject{
+
+	@Override
+	public String getClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
