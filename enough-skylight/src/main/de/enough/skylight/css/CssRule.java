dd3845883fdd460d67e10819e61bedc0e585b9ca
rickyn <rickyn>
2009-12-11 13:47:52 +0000

    css package not needed at the moment

--- enough-skylight/src/main/de/enough/skylight/css/CssRule.java
+++
@@ -1,9 +0,0 @@
-package de.enough.skylight.css;
-
-import de.enough.polish.util.HashMap;
-
-public class CssRule {
-
-	private String selector;
-	private HashMap properties;
-}
--- enough-skylight/src/main/de/enough/skylight/css/Line.java
+++
@@ -1,5 +0,0 @@
-package de.enough.skylight.css;
-
-public class Line {
-
-}
