febcd348e4a98170db5baf87ac150e7afe7dbcef
rickyn <rickyn>
2009-12-17 14:21:46 +0000

    some clearification

--- enough-skylight/test/main/de/enough/skylight/dom/impl/EventTest.java
+++ enough-skylight/test/main/de/enough/skylight/dom/impl/EventTest.java
@@ -7,12 +7,14 @@ import de.enough.skylight.dom.EventListener;
 public class EventTest extends AbstractDomTest implements EventListener {
 
 	public void testAttributeModificationEvent() {
+		System.out.println("Visual test: Check if the following trace of events is sane.");
 		Element element = this.document1.getElementById("id3");
 	
 		element.addEventListener("DOMAttrModified", this, false);
 		element.addEventListener("DOMAttrModified", this, true);
 
 		element.setAttribute("keyblubb", "valuebla");
+		System.out.println("Visual test END.");
 	}
 
 	public void handleEvent(Event evt) {
