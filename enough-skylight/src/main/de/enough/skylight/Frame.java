d01b864e563db8b42e5be2d606e6ad826bf3d226
rickyn <rickyn>
2009-12-17 14:19:50 +0000

    having a single point of entry for JS and Event stuff is nice

--- enough-skylight/src/main/de/enough/skylight/Frame.java
+++ enough-skylight/src/main/de/enough/skylight/Frame.java
@@ -1,10 +1,13 @@
 package de.enough.skylight;
 
+import de.enough.skylight.dom.impl.EventEmitter;
+
 public class Frame {
 
 	private static Frame frame;
 	
 	private JsEngine jsEngine;
+	private EventEmitter eventEmitter;
 	
 	public static Frame getInstance() {
 		if(frame == null) {
@@ -15,9 +18,15 @@ public class Frame {
 	
 	private Frame() {
 		this.jsEngine = new JsEngine();
+		this.eventEmitter = EventEmitter.getInstance();
 	}
 	
 	public JsEngine getJsEngine() {
 		return this.jsEngine;
 	}
+
+	public EventEmitter getEventEmitter() {
+		return this.eventEmitter;
+	}
+	
 }
