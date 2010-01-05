cc48b0ad056449f40844f4d93fc1c9deb190ebe5
rickyn <rickyn>
2010-01-05 16:09:51 +0000

    cleanup move

---
+++ enough-skylight/src/main/de/enough/skylight/js/JsEngine.java
@@ -0,0 +1,32 @@
+package de.enough.skylight.js;
+
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Script;
+import org.mozilla.javascript.Scriptable;
+import de.enough.skylight.dom.impl.DomNodeImpl;
+
+public class JsEngine {
+
+	private Context context;
+	private Scriptable scope;
+	
+	public JsEngine() {
+		this.context = Context.enter();
+		this.context.setOptionOnErrorThrowExeption(true);
+		this.scope = this.context.initStandardObjects();
+		this.scope.put("alert", this.scope, new AlertScriptableObject());
+	}
+	
+	public void runScript(DomNodeImpl target, String scriptText) {
+		Script script = this.context.compileString(scriptText, "test1", 1);
+		Scriptable scriptable = target.getScriptable();
+		scriptable.setParentScope(this.scope);
+		script.exec(this.context, scriptable);
+	}
+
+	public void runFunction(Object onClickFunction) {
+		// TODO Auto-generated method stub
+		
+	}
+}
