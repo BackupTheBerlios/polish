e9b267ff3ca0e93fe8fe3681e2e415063faba4ed
mkoch <mkoch>
2010-01-13 09:01:08 +0000

    - inherit javadoc for getValue method - remove unused field 'separator'

--- build/source/src/de/enough/polish/preprocess/css/attributes/DimensionMapCssAttribute.java
+++ build/source/src/de/enough/polish/preprocess/css/attributes/DimensionMapCssAttribute.java
@@ -43,10 +43,6 @@ import de.enough.polish.preprocess.css.Style;
  */
 public class DimensionMapCssAttribute extends MapCssAttribute {
 	
-	private char separator = '|';
-
-
-
 	/**
 	 * Creates a new instance.
 	 */
@@ -54,15 +50,8 @@ public class DimensionMapCssAttribute extends MapCssAttribute {
 		super();
 	}
 	
-	
-	
-	/**
-	 * Checks and transforms the given CSS value for this attribute.
-	 * 
-	 * @param value the attribute value
-	 * @param environment the environment
-	 * @return the transformed value or the same value if no transformation is required.
-	 * @throws BuildException when a condition is not met or when the value contains conflicting values
+	/* (non-Javadoc)
+	 * @see de.enough.polish.preprocess.css.attributes.MapCssAttribute#getValue(java.lang.String, de.enough.polish.Environment)
 	 */
 	public String getValue(String value, Environment environment ) {
 		if (this.requiresMapping && this.mappingsByName == null) {
@@ -80,7 +69,6 @@ public class DimensionMapCssAttribute extends MapCssAttribute {
 		}
 
 	}
-	
 
 	/**
 	 * Checks and transforms the given dimension CSS value for this attribute.
@@ -158,7 +146,4 @@ public class DimensionMapCssAttribute extends MapCssAttribute {
 	{
 		return generateAnimationSourceCode("DimensionMapCssAnimation", cssAnimation, style, environment);
 	}
-	
-	
-
 }
