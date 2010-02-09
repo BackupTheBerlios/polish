84fe6321d2d09ee2bd0526493e1c3bed8b1747a2
rickyn <rickyn>
2010-02-09 11:34:00 +0000

    added a connection to the JS event system

--- enough-skylight-testrange-client/src/de/enough/skylight/testrange/client/Browser.java
+++ enough-skylight-testrange-client/src/de/enough/skylight/testrange/client/Browser.java
@@ -5,22 +5,46 @@ import de.enough.polish.ui.CommandListener;
 import de.enough.polish.ui.Display;
 import de.enough.polish.ui.Displayable;
 import de.enough.polish.ui.Form;
-import de.enough.polish.ui.Screen;
 import de.enough.polish.ui.StyleSheet;
+import de.enough.skylight.Services;
 import de.enough.skylight.dom.Document;
 import de.enough.skylight.dom.DomNode;
+import de.enough.skylight.dom.impl.EventImpl;
+import de.enough.skylight.dom.impl.EventProcessorListener;
+import de.enough.skylight.dom.impl.NodeListImpl;
 import de.enough.skylight.event.UserEvent;
 import de.enough.skylight.event.UserEventListener;
+import de.enough.skylight.js.JsEngine;
 import de.enough.skylight.renderer.Renderer;
 import de.enough.skylight.renderer.RendererListener;
 import de.enough.skylight.renderer.Viewport;
 import de.enough.skylight.renderer.builder.DocumentBuilder;
 import de.enough.skylight.renderer.builder.ViewportBuilder;
 import de.enough.skylight.renderer.node.CssElement;
-import de.enough.skylight.renderer.node.NodeUtils;
 
 public class Browser extends Form implements CommandListener, RendererListener, UserEventListener{
 
+	class UpdateUIEventProcessorListener implements EventProcessorListener{
+
+		public void handleAboutToDeliverEvent(EventImpl event) {
+			// Empty.
+		}
+		public void handleDeliveredEvent(EventImpl event) {
+			// Empty.
+		}
+		public void handleEventProcessingAborted(EventImpl event) {
+			// Empty.
+		}
+		public void handleEventProcessingStart(EventImpl event, NodeListImpl nodeListImpl) {
+				// Empty.
+		}
+		public void handleEventProcessingStopped(EventImpl event) {
+			// TODO: Check if the event is a MutationEvent or an event like Focus. Only then update the UI.
+			DomNode node = (DomNode)event.getTarget();
+			domModified(node);
+		}
+	}
+	
 	Command cmdRefresh = new Command("Refresh",Command.SCREEN,0);
 	
 	Command cmdExit = new Command("Exit",Command.EXIT,Integer.MAX_VALUE);
@@ -36,13 +60,16 @@ public class Browser extends Form implements CommandListener, RendererListener, 
 	Refresh refresh;
 	
 	Display display;
-	
+
 	public Browser(String url, Display display) {
 		//#style browser
 		super(url);
 		
 		this.viewport = new Viewport();
 		this.viewport.addUserEventListener(this);
+		
+		UpdateUIEventProcessorListener listener = new UpdateUIEventProcessorListener();
+		Services.getInstance().getEventProcessor().addEventProcessorListener(listener);
 		
 		this.documentBuilder = new DocumentBuilder(url);
 		this.viewportBuilder = new ViewportBuilder(this.viewport);
@@ -62,9 +89,12 @@ public class Browser extends Form implements CommandListener, RendererListener, 
 		addCommand(cmdRefresh);
 	}
 	
+	public void domModified(DomNode node) {
+		this.viewport.nodeUpdated(node);
+	}
+
 	public void showNotify() {
 		super.showNotify();
-		
 		if(this.renderer.getState() == Renderer.STATE_VOID) {
 			this.renderer.render();
 		}
@@ -78,6 +108,13 @@ public class Browser extends Form implements CommandListener, RendererListener, 
 		if(state == Renderer.STATE_READY) {
 			this.display.setCurrent(this);
 		} 
+		
+		if(state == Renderer.STATE_BUILD_VIEW) {
+			Services service = Services.getInstance();
+			JsEngine jsEngine = service.getJsEngine();
+			Document document = this.viewportBuilder.getDocument();
+			jsEngine.setDocument(document);
+		}
 	}
 	
 	public void commandAction(Command command, Displayable screen) {
@@ -91,16 +128,8 @@ public class Browser extends Form implements CommandListener, RendererListener, 
 	}
 	
 	public void onUserEvent(CssElement element, UserEvent event) {
-Document document = this.viewportBuilder.getDocument();
-		DomNode node = document.getElementById("content");
-		String clazz = NodeUtils.getAttributeValue(node, "class");
-		
-		if(clazz.equals("content")) {
-			NodeUtils.setAttributeValue(node, "class", "contenthide");
-		} else {
-			NodeUtils.setAttributeValue(node, "class", "content");
-		}
-		
-		this.viewport.nodeUpdated(node);
+		DomNode node = element.getNode();
+		Services.getInstance().getEventEmitter().emitClickEvent(node, 0, 0);
 	}
+	
 }
