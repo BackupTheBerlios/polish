e143befe8c1ed476fbc95330e47b33a3486af682
mbode <mbode>
2010-03-09 15:10:47 +0000

    It was not possible to compile, when you was using usePolishGui = false

--- enough-polish-j2me/source/src/de/enough/polish/ui/gaugeviews/ClockInstrumentGaugeView.java
+++ enough-polish-j2me/source/src/de/enough/polish/ui/gaugeviews/ClockInstrumentGaugeView.java
@@ -1,4 +1,4 @@
-//#condition polish.hasFloatingPoint && !polish.android && !polish.blackberry
+//#condition polish.usePolishGui && polish.hasFloatingPoint && !polish.android && !polish.blackberry
 /*
  * Copyright (c) 2004-2005 Robert Virkus / Enough Software
  *
