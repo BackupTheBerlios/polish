61b8836b62105eb8e7b7587bc7eb7d2ba12117b4
enough <enough>
2010-12-15 23:30:55 +0000

    added application stub to blank

--- enough-polish-sample-blank/source/src/de/enough/polish/app/view/MainMenuList.java
+++ enough-polish-sample-blank/source/src/de/enough/polish/app/view/MainMenuList.java
@@ -1,7 +1,7 @@
 /*
- * Created on Mar 13, 2008 at 12:58:57 PM.
+ * Created on Dec 15, 2010 at 6:49:51 PM.
  * 
- * Copyright (c) 2005 Robert Virkus / Enough Software
+ * Copyright (c) 2010 Robert Virkus / Enough Software
  *
  * This file is part of J2ME Polish.
  *
@@ -23,31 +23,34 @@
  * refer to the accompanying LICENSE.txt or visit
  * http://www.j2mepolish.org for details.
  */
-package de.enough.polish.sample;
+package de.enough.polish.app.view;
 
-import javax.microedition.midlet.MIDlet;
+import de.enough.polish.ui.List;
+import de.enough.polish.util.Locale;
 
 /**
- * 
- * <br>Copyright Enough Software 2005-2008
- * <pre>
- * history
- *        Mar 13, 2008 - rickyn creation
- * </pre>
- * @author Richard Nkrumah
+ * <p>The main menu of the application.</p>
+ * <p>Often you can use List directly instead of using a specialized class.</p>
+ *
+ * <p>Copyright Enough Software 2010</p>
+ * @author Robert Virkus, j2mepolish@enough.de
  */
-public class Blank extends MIDlet{
+public class MainMenuList extends List {
 
-    protected void destroyApp(boolean arg0) {
-        System.out.println("Destroying MIDlet.");
-    }
-
-    protected void pauseApp() {
-        System.out.println("Stopping MIDlet.");
-    }
-
-    protected void startApp(){
-        System.out.println("Starting MIDlet.");
-    }
-
+	/**
+	 * Creates a new main menu screen.
+	 */
+	public MainMenuList() {
+		//#style screenMainMenu
+		super( Locale.get("main.title"), List.IMPLICIT);
+	}
+	
+	/**
+	 * Adds a main menu entry to this screen.
+	 * @param name the name of the entry
+	 */
+	public void addEntry( String name ) {
+		//#style itemMainMenuEntry
+		append(name, null);
+	}
 }
