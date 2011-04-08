aa301c3ec2a41df61591ebbc01c7c67d74de4cd6
oiliescu <oiliescu>
2011-04-08 14:30:32 +0000

    Added Processing Tree sample project.

---
+++ enough-polish-sample-processing-tree/src/de/enough/polish/sample/processing/tree/ProcessingTreeMidlet.java
@@ -0,0 +1,85 @@
+/*
+ * Created on 26-June-2007 at 16:14:27.
+ *
+ * Copyright (c) 2007 Robert Virkus / Enough Software
+ *
+ * This file is part of J2ME Polish.
+ *
+ * J2ME Polish is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * J2ME Polish is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with J2ME Polish; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Commercial licenses are also available, please
+ * refer to the accompanying LICENSE.txt or visit
+ * http://www.j2mepolish.org for details.
+ */
+
+package de.enough.polish.sample.processing.tree;
+
+import de.enough.polish.processing.ProcessingContext;
+import de.enough.polish.processing.ProcessingScreen;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+import de.enough.polish.util.DeviceControl;
+
+//#ifdef polish.debugEnabled
+//#endif
+
+/**
+ * <p>Shows a demonstration of the possibilities of J2ME Polish.</p>
+ *
+ * <p>Copyright Enough Software 2007</p>
+
+ * <pre>
+ * history
+ *        26-June-2007 - rob creation
+ * </pre>
+ * @author Robert Virkus, j2mepolish@enough.de
+ */
+public class ProcessingTreeMidlet extends MIDlet{
+
+	ProcessingScreen screen ;
+    Display display;
+
+	public ProcessingTreeMidlet() {
+		super();
+
+        // Create a sample ProcessingContext
+        ProcessingContext context = new ProcessingTreeContext();
+
+        // And attach it to a ProcessingScreen.
+		//#style mainScreen
+		this.screen = new ProcessingScreen("Processing Tree", context);                
+	}
+
+	protected void startApp() throws MIDletStateChangeException {		
+		this.display = Display.getDisplay(this);
+		this.display.setCurrent( this.screen );		
+	}
+
+	protected void pauseApp() {
+	}
+
+	protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+		// just quit
+	}
+
+	private void quit() {
+		notifyDestroyed();
+	}
+
+}
